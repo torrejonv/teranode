68d4246fd643a46ff6b267999e744ddbfff4b491
ordishs <71426+ordishs@users.noreply.github.com>
2023-03-24 15:53:48 +0100

    Test to prove stream transaction data

--- services/propagation/peer_handler.go
+++ services/propagation/peer_handler.go
@@ -1,20 +1,30 @@
 package propagation
 
 import (
+	"bytes"
+	"fmt"
+	"time"
+
 	"github.com/libsv/go-p2p"
+	"github.com/libsv/go-p2p/blockchain"
+	"github.com/libsv/go-p2p/chaincfg/chainhash"
 	"github.com/libsv/go-p2p/wire"
 	"github.com/ordishs/go-utils"
+	"github.com/ordishs/go-utils/batcher"
 	"github.com/ordishs/gocore"
 )
 
 type PeerHandler struct {
-	logger utils.Logger
+	logger                utils.Logger
+	getTransactionBatcher *batcher.Batcher[chainhash.Hash]
 }
 
 func NewPeerHandler() p2p.PeerHandlerI {
-	return &PeerHandler{
+	ph := &PeerHandler{
 		logger: gocore.Log("p2p"),
 	}
+
+	return ph
 }
 
 func (ph *PeerHandler) HandleTransactionGet(msg *wire.InvVect, peer p2p.PeerI) ([]byte, error) {
@@ -39,11 +49,48 @@ func (ph *PeerHandler) HandleTransaction(msg *wire.MsgTx, peer p2p.PeerI) error 
 	return nil
 }
 
-func (ph *PeerHandler) HandleBlockAnnouncement(msg *wire.InvVect, peer p2p.PeerI) error {
-	ph.logger.Infof("received block inv: %v", msg.Hash.String())
+func (ph *PeerHandler) HandleBlockAnnouncement(invMsg *wire.InvVect, peer p2p.PeerI) error {
+	ph.logger.Infof("received block inv: %v", invMsg.Hash.String())
+
+	msg := wire.NewMsgGetData()
+
+	if err := msg.AddInvVect(wire.NewInvVect(wire.InvTypeBlock, &invMsg.Hash)); err != nil {
+		ph.logger.Errorf("ProcessBlock: could not create InvVect: %v", err)
+		return err
+	}
+
+	if err := peer.WriteMsg(msg); err != nil {
+		ph.logger.Errorf("ProcessBlock: failed to write message to peer: %v", err)
+		return err
+	}
+
+	ph.logger.Infof("ProcessBlock: %s", invMsg.Hash.String())
+
 	return nil
 }
 
 func (ph *PeerHandler) HandleBlock(msg *p2p.BlockMessage, peer p2p.PeerI) error {
+	start := time.Now()
+
+	blockHash := msg.Header.BlockHash()
+
+	// previousBlockHash := msg.Header.PrevBlock
+
+	merkleRoot := msg.Header.MerkleRoot
+
+	transactionHashes := make([][]byte, len(msg.TransactionHashes))
+	for i, hash := range msg.TransactionHashes {
+		transactionHashes[i] = hash[:]
+
+		peer.RequestTransaction(hash)
+	}
+
+	calculatedMerkleRoot := blockchain.BuildMerkleTreeStore(transactionHashes)
+	if !bytes.Equal(calculatedMerkleRoot[len(calculatedMerkleRoot)-1], merkleRoot[:]) {
+		return fmt.Errorf("merkle root mismatch for block %s", blockHash.String())
+	}
+
+	ph.logger.Infof("Processed block %s, %d transactions in %0.2f seconds", blockHash.String(), len(msg.TransactionHashes), time.Since(start).Seconds())
+
 	return nil
 }
---
+++ services/validator/Validator_test.go
@@ -0,0 +1,91 @@
+package validator
+
+import (
+	"context"
+	"encoding/hex"
+	"testing"
+
+	"github.com/TAAL-GmbH/ubs/services/validator/validator_api"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type TestLogger struct {
+	t *testing.T
+}
+
+func NewTestLogger(t *testing.T) *TestLogger {
+	return &TestLogger{
+		t: t,
+	}
+}
+
+func (l *TestLogger) LogLevel() int {
+	return 0
+}
+
+func (l *TestLogger) Debugf(format string, args ...interface{}) {
+	l.t.Logf(format, args...)
+}
+
+func (l *TestLogger) Infof(format string, args ...interface{}) {
+	l.t.Logf(format, args...)
+}
+
+func (l *TestLogger) Warnf(format string, args ...interface{}) {
+	l.t.Logf(format, args...)
+}
+
+func (l *TestLogger) Errorf(format string, args ...interface{}) {
+	l.t.Logf(format, args...)
+}
+
+func (l *TestLogger) Fatalf(format string, args ...interface{}) {
+	l.t.Logf(format, args...)
+}
+
+func TestGRPCStreaming(t *testing.T) {
+	tx, _ := hex.DecodeString("01000000016ec78dc364a3a911b38b32e58e5c228030f172da7d550e699939f6f3771f7f63010000006b483045022100dc6378291c4f8e06a54b0b60701cff7719c985db20537cd16d0350abe2f749660220694d67764dd8501e32190e6209a48103e3c7b202bb8c7682fc4c86e890c58409412103c3e59e22c5a32e54183a43993e8f584f709785f440fbf6f960551cb32041c5a9ffffffff03d0070000000000001976a9149e10b4a781c5be9f67367c3994fb3419aafd358e88ac2a240c00000000001976a914c52f8797b57f0b0cfc5856a5dd4a6f491a41822c88ac00000000000000000a006a075354554b2e434f00000000")
+
+	logger := NewTestLogger(t)
+
+	// Start the Validator server
+	validator := NewServer(logger)
+
+	go func() {
+		err := validator.Start()
+		require.NoError(t, err)
+	}()
+
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+
+	conn, err := grpc.Dial("localhost:8011", opts...)
+	require.NoError(t, err)
+
+	defer conn.Close()
+
+	client := validator_api.NewValidatorAPIClient(conn)
+
+	stream, err := client.ValidateTransaction(context.Background())
+	require.NoError(t, err)
+
+	err = stream.Send(&validator_api.ValidateTransactionRequest{
+		TransactionData: tx[:50],
+	})
+	require.NoError(t, err)
+
+	err = stream.Send(&validator_api.ValidateTransactionRequest{
+		TransactionData: tx[50:],
+	})
+	require.NoError(t, err)
+
+	res, err := stream.CloseAndRecv()
+	require.NoError(t, err)
+
+	t.Log(res)
+
+	validator.Stop(context.Background())
+}
