59691ecbcffd34138510717cdc33fe6fd543e6e6
ordishs <71426+ordishs@users.noreply.github.com>
2023-05-09 09:33:27 +0200

    Added imports

--- main_native.go
+++ main_native.go
@@ -3,6 +3,14 @@
 
 package main
 
+import (
+	"log"
+
+	"github.com/libsv/go-bt/v2/bscript/interpreter"
+	"github.com/ordishs/gocore"
+	"github.com/ordishs/verifysignature"
+)
+
 func init() {
 	if gocore.Config().GetBool("use_cgo_verifier", false) {
 		log.Println("Using CGO verifier")
