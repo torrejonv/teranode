e586a95b9ba716c29a3acfa4552fbe4cf6ccf8e7
ordishs <71426+ordishs@users.noreply.github.com>
2023-05-10 10:02:43 +0200

    Turn on CGO validation

--- scripts/cluster_start_all.sh
+++ scripts/cluster_start_all.sh
@@ -4,16 +4,16 @@ kubectl scale deployment -n seeder-service --replicas 1 --all
 
 kubectl scale deployment -n utxostore-service --replicas 1 --all
 
-kubectl scale deployment -n validation-service --replicas 5 --all
+kubectl scale deployment -n validation-service --replicas 4 --all
 
 echo "Sleeping for 30 seconds to allow all validation services to spin up"
 sleep 30
 echo "Starting propagation services"
 
-kubectl scale deployment -n propagation-service --replicas 2 --all
-sleep 4
+kubectl scale deployment -n propagation-service --replicas 4 --all
+sleep 30
 
 
-kubectl scale deployment -n tx-blaster-service --replicas 2 --all
+kubectl scale deployment -n tx-blaster-service --replicas 4 --all
 
 kubectl get pods --all-namespaces -o wide | grep "-service"
