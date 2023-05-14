5c73176c433bd8e31084d4bd90d0271c866dd9bb
Siggi <4411176+icellan@users.noreply.github.com>
2023-05-14 15:19:21 +0200

    Changed creating of context for native C signature functions

--- main_native.go
+++ main_native.go
@@ -11,48 +11,16 @@ package main
 import "C"
 import (
 	"log"
-	"unsafe"
 
+	"github.com/TAAL-GmbH/ubsv/native"
 	"github.com/libsv/go-bt/v2/bscript/interpreter"
 	"github.com/ordishs/gocore"
 )
 
 func init() {
+	// Create a secp256k1 context
 	if gocore.Config().GetBool("use_cgo_verifier", false) {
 		log.Println("Using CGO verifier - VerifySignature")
-		interpreter.InjectExternalVerifySignatureFn(VerifySignature)
+		interpreter.InjectExternalVerifySignatureFn(native.VerifySignature)
 	}
-}
-
-func VerifySignature(message []byte, signature []byte, publicKey []byte) bool {
-	// Create a secp256k1 context
-	ctx := C.secp256k1_context_create(C.SECP256K1_CONTEXT_SIGN | C.SECP256K1_CONTEXT_VERIFY)
-	defer C.free(unsafe.Pointer(ctx))
-
-	// Allocate memory for the message, signature, and public key
-	cMessage := C.CBytes(message)
-	defer C.free(cMessage)
-	cSignature := C.CBytes(signature)
-	defer C.free(cSignature)
-	cPublicKey := C.CBytes(publicKey)
-	defer C.free(cPublicKey)
-
-	// Create a secp256k1 signature object
-	var cSig C.secp256k1_ecdsa_signature
-	if C.secp256k1_ecdsa_signature_parse_der(ctx, &cSig, (*C.uchar)(cSignature), C.size_t(len(signature))) != 1 {
-		return false
-	}
-
-	// Create a secp256k1 public key object
-	var cPubKey C.secp256k1_pubkey
-	if C.secp256k1_ec_pubkey_parse(ctx, &cPubKey, (*C.uchar)(cPublicKey), C.size_t(len(publicKey))) != 1 {
-		return false
-	}
-
-	// Verify the signature
-	if C.secp256k1_ecdsa_verify(ctx, &cSig, (*C.uchar)(cMessage), &cPubKey) != 1 {
-		return false
-	}
-
-	return true
 }
--- native/signature_test.go
+++ native/signature_test.go
@@ -1,6 +1,6 @@
 //go:build native
 
-package main
+package native
 
 import (
 	"encoding/hex"
