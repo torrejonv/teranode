311f6571a5ce630d49dcd7e7592e7c733ef47f1d
ordishs <71426+ordishs@users.noreply.github.com>
2023-06-13 11:59:42 +0200

    Added GetBlockSubsidy

---
+++ util/BlockSubsidy.go
@@ -0,0 +1,16 @@
+package util
+
+// GetBlockSubsidyForHeight func
+func GetBlockSubsidyForHeight(height uint64) uint64 {
+	halvings := height / 210000
+	// Force block reward to zero when right shift is undefined.
+	if halvings >= 64 {
+		return 0
+	}
+
+	subsidy := uint64(50 * 1e8)
+
+	// Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
+	subsidy >>= halvings
+	return subsidy
+}
