34cd456985fa1b04c88ead0c7c1315e671e7154f
Siggi <4411176+icellan@users.noreply.github.com>
2023-06-14 13:32:50 +0200

    Implemented blockchain store

--- stores/blockchain/new.go
+++ stores/blockchain/new.go
@@ -1,11 +1,22 @@
 package blockchain
 
 import (
+	"fmt"
 	"net/url"
 
+	"github.com/TAAL-GmbH/ubsv/stores/blockchain/sql"
 	"github.com/ordishs/go-utils"
 )
 
-func NewStore(logger utils.Logger, storeUrl *url.URL) (Store, error) {
-	return nil, nil
+func NewStore(_ utils.Logger, storeUrl *url.URL) (Store, error) {
+	switch storeUrl.Scheme {
+	case "postgres":
+		fallthrough
+	case "sqlitememory":
+		fallthrough
+	case "sqlite":
+		return sql.New(storeUrl)
+	}
+
+	return nil, fmt.Errorf("unknown scheme: %s", storeUrl.Scheme)
 }
