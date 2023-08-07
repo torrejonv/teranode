a9144e7c78c3517a3b076ae7b38b24d3c3e29de0
Qua Is X <109706571+quaisx@users.noreply.github.com>
2023-08-07 14:43:57 -0400

    [bug] nil in Equal - handle

--- db/model/block.go
+++ db/model/block.go
@@ -12,6 +12,9 @@ type Block struct {
 }
 
 func (b *Block) Equal(other *Block) bool {
+	if b == nil || other == nil {
+		return false
+	}
 	return b.Height == other.Height &&
 		b.BlockHash == other.BlockHash &&
 		b.PrevBlockHash == other.PrevBlockHash
--- services/coinbasetracker/Util.go
+++ services/coinbasetracker/Util.go
@@ -16,9 +16,7 @@ func IsGenesisBlock(block *smodel.Block) bool {
 	// merkleRoot := "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
 	// Note: genesis block hash is different from merkleRoot
 	// Need to know the genesis block hash.
-	return block.Height == 0 &&
-		// block.BlockHash == merkleRoot &&
-		block.PrevBlockHash == previousBlock.String()
+	return block.PrevBlockHash == previousBlock.String()
 }
 
 func NetworkBlockToStoreBlock(nblock *nmodel.Block) (*smodel.Block, error) {
