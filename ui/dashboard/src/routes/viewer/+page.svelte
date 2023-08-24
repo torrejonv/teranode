6c5e3bc8382fc3bd70063882b02dbff3bd8af079
ordishs <71426+ordishs@users.noreply.github.com>
2023-08-24 19:30:23 +0200

    reactive url

--- ui/dashboard/src/routes/viewer/+page.svelte
+++ ui/dashboard/src/routes/viewer/+page.svelte
@@ -1,16 +1,16 @@
 <script>
-	let id = '';
+	let hash = '';
 	let type = '';
 	let data = null;
 	let addr = 'https://miner1.ubsv.dev:8090';
 	let error = null;
 
+	$: url = addr + '/' + type + '/' + hash + '/json';
+
 	const itemTypes = ['tx', 'subtree', 'header', 'block', 'utxo'];
 
 	async function fetchData() {
 		try {
-			const url = `${addr}/${type}/${id}/json`;
-			console.log(url);
 			const response = await fetch(url);
 			if (!response.ok) {
 				throw new Error(`HTTP error! Status: ${response.status}`);
@@ -23,39 +23,53 @@
 </script>
 
 <section class="section">
-	<h1 class="title">Enter ID and Select Item Type to Fetch Data</h1>
-	<div class="field is-horizontal">
-		<div class="field-label is-normal">
-			<label class="label">Item Type</label>
+	<div class="field">
+		<p>Select the type and enter the hash to fetch data.</p>
+	</div>
+
+	<div class="field">
+		<label class="label"
+			>HTTP Address
+			<div class="control">
+				<input class="input" type="text" bind:value={addr} placeholder="Enter HTTP address" />
+			</div>
+		</label>
+	</div>
+
+	<div class="field">
+		<label class="label"
+			>Type
+			<div class="control">
+				<div class="select">
+					<select bind:value={type}>
+						<option value="">-- Select Type --</option>
+						{#each itemTypes as type}
+							<option value={type}>{type}</option>
+						{/each}
+					</select>
+				</div>
+			</div>
+		</label>
+	</div>
+
+	<div class="field">
+		<label class="label"
+			>Hash
+			<div class="control">
+				<input class="input hex-input" type="text" bind:value={hash} placeholder="Enter ID" />
+			</div>
+		</label>
+	</div>
+
+	<div class="field">
+		<div class="control">
+			{url}
 		</div>
-		<div class="field-body">
-			<div class="field">
-				<div class="control">
-					<div class="select">
-						<select bind:value={type}>
-							<option value="">-- Select Item Type --</option>
-							{#each itemTypes as type}
-								<option value={type}>{type}</option>
-							{/each}
-						</select>
-					</div>
-				</div>
-			</div>
-			<div class="field">
-				<div class="control">
-					<input class="input" type="text" bind:value={addr} placeholder="Enter HTTP address" />
-				</div>
-			</div>
-			<div class="field">
-				<div class="control">
-					<input class="input hex-input" type="text" bind:value={id} placeholder="Enter ID" />
-				</div>
-			</div>
-			<div class="field">
-				<div class="control">
-					<button class="button is-primary" on:click={fetchData}>Fetch</button>
-				</div>
-			</div>
+	</div>
+
+	<div class="field">
+		<div class="control">
+			<button class="button is-primary" on:click={fetchData}>Fetch</button>
 		</div>
 	</div>
 
@@ -66,14 +80,13 @@
 			<pre>
         {JSON.stringify(data, null, 2)}
       </pre>
-			<!-- Display the fetched data here. Adjust based on your data structure. -->
 		</div>
 	{/if}
 </section>
 
 <style>
 	.hex-input {
-		width: 600px; /* Adjust this value based on the font and size you're using */
-		max-width: 100%; /* Ensure it doesn't overflow on smaller screens */
+		width: 650px;
+		max-width: 100%;
 	}
 </style>
