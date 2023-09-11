a79d8278bcbd82bb898584cbc6b2064f740257cc
Siggi <4411176+icellan@users.noreply.github.com>
2023-09-11 15:06:57 +0200

    Added kafka docker shell script, added config options for kafka

---
+++ deploy/dev/kafka.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+docker network create app-tier --driver bridge
+
+docker run -d -p 9092-9093:9092-9093 --name kafka-server --hostname localhost \
+    --network app-tier \
+    -e ALLOW_PLAINTEXT_LISTENER=yes \
+    -e KAFKA_CFG_NODE_ID=0 \
+    -e KAFKA_CFG_PROCESS_ROLES=controller,broker \
+    -e KAFKA_CFG_LISTENERS=PLAINTEXT://:9092,CONTROLLER://:9093 \
+    -e KAFKA_CFG_LISTENER_SECURITY_PROTOCOL_MAP=CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT \
+    -e KAFKA_CFG_CONTROLLER_QUORUM_VOTERS=0@kafka-server:9093 \
+    -e KAFKA_CFG_CONTROLLER_LISTENER_NAMES=CONTROLLER \
+    bitnami/kafka:latest
