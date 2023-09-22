51b64689a6824e934f6996c35452f5b4aa732b69
ordishs <71426+ordishs@users.noreply.github.com>
2023-09-22 10:46:59 +0200

    New URL for iframe

--- ui/dashboard/src/routes/metrics/+page.svelte
+++ ui/dashboard/src/routes/metrics/+page.svelte
@@ -1,7 +1,16 @@
 <!-- svelte-ignore a11y-missing-attribute -->
-<iframe
-  src="https://grafana.ubsv.dev/d-solo/cde0cde9-5698-4433-b851-ccae709ee2b2/ubsv-service-overview?orgId=1&refresh=5s&from=1695326662341&to=1695330262341&theme=light&panelId=2"
-  width="450"
-  height="200"
-  frameborder="0"
-/>
+<div class="full">
+  <iframe
+    src="https://grafana.ubsv.dev/d-solo/cde0cde9-5698-4433-b851-ccae709ee2b2/ubsv-service-overview?orgId=1&refresh=5s&from=1695368621683&to=1695372221683&theme=light&panelId=2"
+    width="100%"
+    height="100%"
+    frameborder="0"
+  />
+</div>
+
+<style>
+  .full {
+    width: 100%;
+    height: 100vh;
+  }
+</style>
