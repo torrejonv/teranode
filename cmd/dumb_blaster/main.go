8b410235ccefeb8c38e487a43eb0c241960ed858
ordishs <71426+ordishs@users.noreply.github.com>
2023-09-27 12:21:18 +0200

    Added broadcast option

--- cmd/dumb_blaster/main.go
+++ cmd/dumb_blaster/main.go
@@ -4,28 +4,49 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"strings"
 	"sync/atomic"
 	"time"
 
 	"github.com/bitcoin-sv/ubsv/native"
+	"github.com/bitcoin-sv/ubsv/services/propagation/propagation_api"
+	"github.com/bitcoin-sv/ubsv/util"
 	"github.com/libsv/go-bk/wif"
 	"github.com/libsv/go-bt/v2"
 	"github.com/libsv/go-bt/v2/unlocker"
+	"github.com/ordishs/gocore"
 )
 
 var (
 	incompleteTx, _ = bt.NewTxFromString("010000000000000000ef0193a35408b6068499e0d5abd799d3e827d9bfe70c9b75ebe209c91d25072326510000000000ffffffff00e1f505000000001976a914c0a3c167a28cabb9fbb495affa0761e6e74ac60d88ac02404b4c00000000001976a91404ff367be719efa79d76e4416ffb072cd53b208888acde94a905000000001976a91404d03f746652cfcb6cb55119ab473a045137d26588ac00000000")
 	w               *wif.WIF
 	counter         atomic.Int64
+	workerCount     int
+	broadcast       bool
+	client          propagation_api.PropagationAPIClient
 )
 
 func init() {
 	unlocker.InjectExternalSignerFn(native.SignMessage)
+
+	propagationServerAddr, _ := gocore.Config().GetMulti("propagation_grpcAddresses", "|")
+	conn, err := util.GetGRPCClient(context.Background(), propagationServerAddr[0], &util.ConnectionOptions{
+		Prometheus: gocore.Config().GetBool("use_prometheus_grpc_metrics", true),
+		MaxRetries: 3,
+	})
+	if err != nil {
+		panic(err)
+	}
+	client = propagation_api.NewPropagationAPIClient(conn)
 }
 
 func main() {
+	flag.IntVar(&workerCount, "workers", 1, "Set worker count")
+	flag.BoolVar(&broadcast, "broadcast", false, "Broadcast to propagation server")
+	flag.Parse()
+
 	var err error
 
 	w, err = wif.DecodeWIF("cNGwGSc7KRrTmdLUZ54fiSXWbhLNDc2Eg5zNucgQxyQCzuQ5YRDq")
@@ -44,7 +65,7 @@ func main() {
 		}
 	}()
 
-	for i := 0; i < 12; i++ {
+	for i := 0; i < workerCount; i++ {
 		go worker()
 	}
 
@@ -61,8 +82,22 @@ func worker() {
 			panic(err)
 		}
 
+		if broadcast {
+			if err := sendToPropagationServer(context.Background(), tx.ExtendedBytes()); err != nil {
+				panic(err)
+			}
+		}
+
 		counter.Add(1)
 	}
+}
+
+func sendToPropagationServer(ctx context.Context, txExtendedBytes []byte) error {
+	_, err := client.ProcessTransaction(ctx, &propagation_api.ProcessTransactionRequest{
+		Tx: txExtendedBytes,
+	})
+
+	return err
 }
 
 func FormatFloat(f float64) string {
