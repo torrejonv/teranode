22bf0faea5904744186ec9203b1e5fe815924029
teracoder <199657068+teracoder@users.noreply.github.com>
2023-10-02 21:10:01 +0100

    [Fix] grafana url

--- ui/dashboard/src/routes/metrics/+page.svelte
+++ ui/dashboard/src/routes/metrics/+page.svelte
@@ -1,7 +1,7 @@
 <!-- svelte-ignore a11y-missing-attribute -->
 <div class="full">
   <iframe
-    src="https://grafana.ubsv.dev/d-solo/cde0cde9-5698-4433-b851-ccae709ee2b2/ubsv-service-overview?orgId=1&refresh=5s&from=1695368621683&to=1695372221683&theme=light&panelId=2"
+    src="https://grafana.ubsv.dev/d-solo/cde0cde9-5698-4433-b851-ccae709ee2b2/ubsv-service-overview?orgId=1&refresh=5s&panelId=2"
     width="100%"
     height="100%"
     frameborder="0"
