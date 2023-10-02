eb6e8fa881a4bdd2ccb2c52305410ecd3e2fff33
ordishs <71426+ordishs@users.noreply.github.com>
2023-10-02 11:20:59 +0100

    Again

--- ui/dashboard/src/stores/bootstrapStore.js
+++ ui/dashboard/src/stores/bootstrapStore.js
@@ -81,7 +81,7 @@ export function connectToBootstrap(blobServerHTTPAddress) {
 
         console.log('BootstrapWS', json)
         let nodesData = get(nodes)
-        if (!nodesData.find((node) => node.name === json.names)) {
+        if (!nodesData.find((node) => node.name === json.name)) {
           nodesData.push(json)
         }
 
