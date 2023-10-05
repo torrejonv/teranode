a9077ddc9419e773f6bf6bc99adf88d76e45d4c8
ordishs <71426+ordishs@users.noreply.github.com>
2023-10-05 18:15:36 +0200

    Settings for frpc

--- scripts/scale_all.sh
+++ scripts/scale_all.sh
@@ -11,3 +11,12 @@ ksd utxostore1 --replicas 0 --all
 
 ksd tx-blaster1 --replicas 1 --all
 ksd propagation1 --replicas 1 --all
+
+
+# Blockchain must be first (UTXOStore uses it)
+ksd blockchain1 --replicas 1 --all
+
+ksd txmetastore1 --replicas 1 --all
+ksd utxostore1 --replicas 1 --all
+ksd blob1 --replicas 1 --all
+ksd blockassembly1 --replicas 1 --all
