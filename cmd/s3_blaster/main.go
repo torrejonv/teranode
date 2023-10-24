7705a0760f3640b60fb99d72f980158ab48855e3
freemans13 <freemans13@users.noreply.github.com>
2023-10-24 17:46:39 +0100

    Stop using blob.Store factory (kept iniing badger even tho I'm not using it)

--- cmd/s3_blaster/main.go
+++ cmd/s3_blaster/main.go
@@ -16,6 +16,16 @@ import (
 
 	_ "github.com/bitcoin-sv/ubsv/k8sresolver"
 	"github.com/bitcoin-sv/ubsv/stores/blob"
+	"github.com/bitcoin-sv/ubsv/stores/blob/file"
+	"github.com/bitcoin-sv/ubsv/stores/blob/gcs"
+	"github.com/bitcoin-sv/ubsv/stores/blob/kinesiss3"
+	"github.com/bitcoin-sv/ubsv/stores/blob/memory"
+	"github.com/bitcoin-sv/ubsv/stores/blob/minio"
+	"github.com/bitcoin-sv/ubsv/stores/blob/null"
+	"github.com/bitcoin-sv/ubsv/stores/blob/s3"
+	"github.com/bitcoin-sv/ubsv/stores/blob/seaweedfs"
+	"github.com/bitcoin-sv/ubsv/stores/blob/seaweedfss3"
+	"github.com/bitcoin-sv/ubsv/stores/blob/sql"
 	"github.com/ordishs/go-utils"
 	"github.com/ordishs/gocore"
 )
@@ -93,7 +103,7 @@ func main() {
 }
 
 func worker(logger utils.Logger) {
-	txStore, err := blob.NewStore(txStoreUrl)
+	txStore, err := NewStore(txStoreUrl)
 	if err != nil {
 		panic(err)
 	}
@@ -164,4 +174,62 @@ func calculatePrefix(s3Key []byte) []byte {
 
 	// Combine the prefix and the key with a "/"
 	return append(prefixBytes, append(separator, s3Key...)...)
+}
+
+func NewStore(storeUrl *url.URL) (store blob.Store, err error) {
+	switch storeUrl.Scheme {
+	case "null":
+		store, err = null.New()
+		if err != nil {
+			return nil, fmt.Errorf("error creating null blob store: %v", err)
+		}
+	case "memory":
+		store = memory.New()
+	case "file":
+		store, err = file.New("." + storeUrl.Path) // relative
+		if err != nil {
+			return nil, fmt.Errorf("error creating file blob store: %v", err)
+		}
+	case "postgres", "sqlite", "sqlitememory":
+		store, err = sql.New(storeUrl)
+		if err != nil {
+			return nil, fmt.Errorf("error creating sql blob store: %v", err)
+		}
+	case "gcs":
+		store, err = gcs.New(strings.Replace(storeUrl.Path, "/", "", 1))
+		if err != nil {
+			return nil, fmt.Errorf("error creating gcs blob store: %v", err)
+		}
+	case "minio":
+		fallthrough
+	case "minios":
+		store, err = minio.New(storeUrl)
+		if err != nil {
+			return nil, fmt.Errorf("error creating minio blob store: %v", err)
+		}
+	case "s3":
+		store, err = s3.New(storeUrl)
+		if err != nil {
+			return nil, fmt.Errorf("error creating s3 blob store: %v", err)
+		}
+	case "kinesiss3":
+		store, err = kinesiss3.New(storeUrl)
+		if err != nil {
+			return nil, fmt.Errorf("error creating kinesiss3 blob store: %v", err)
+		}
+	case "seaweedfs":
+		store, err = seaweedfs.New(storeUrl)
+		if err != nil {
+			return nil, fmt.Errorf("error creating seaweedfs blob store: %v", err)
+		}
+	case "seaweedfss3":
+		store, err = seaweedfss3.New(storeUrl)
+		if err != nil {
+			return nil, fmt.Errorf("error creating seaweedfss3 blob store: %v", err)
+		}
+	default:
+		return nil, fmt.Errorf("unknown store type: %s", storeUrl.Scheme)
+	}
+
+	return
 }
