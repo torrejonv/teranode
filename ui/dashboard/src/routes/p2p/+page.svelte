91866a608281d70f528d90c32ac4c60e764531d8
ordishs <71426+ordishs@users.noreply.github.com>
2023-10-28 02:21:51 +0200

    New filtering

--- ui/dashboard/src/routes/p2p/+page.svelte
+++ ui/dashboard/src/routes/p2p/+page.svelte
@@ -17,21 +17,118 @@
   afterUpdate(() => {
     scrollToTop()
   })
+
+  let byPeer = false
+  let filter = ''
+  let groupedMessages = []
+  let filteredMessages = []
+
+  $: {
+    filteredMessages = $messages
+
+    if (filter.length > 0) {
+      const f = filter.toLowerCase()
+
+      filteredMessages = filteredMessages.filter((message) => {
+        const search = JSON.stringify(message).toLowerCase()
+        return search.includes(f)
+      })
+    }
+
+    if (byPeer) {
+      let newGroupedMessages = {}
+
+      filteredMessages.forEach((message) => {
+        if (message.type !== 'Ping') {
+          if (!newGroupedMessages[message.peer_id]) {
+            newGroupedMessages[message.peer_id] = []
+          }
+          newGroupedMessages[message.peer_id].push(message)
+        }
+      })
+
+      Object.keys(newGroupedMessages).forEach((peer_id) => {
+        newGroupedMessages[peer_id].sort((a, b) =>
+          a.peer_id.localeCompare(b.peerid)
+        )
+      })
+
+      groupedMessages = newGroupedMessages
+    }
+  }
 </script>
 
 <div>
-  <div class="url">{$wsUrl}</div>
+  <div class="url">
+    {$wsUrl}
+    <span class="check">
+      <input type="checkbox" bind:checked={byPeer} id="group-by-peer" />
+      <label for="group-by-peer" class="checkbox-label">by Peer</label>
+      <input
+        class="input is-small"
+        type="text"
+        placeholder="Filter"
+        bind:value={filter}
+      />
+    </span>
+  </div>
 
-  {#each $messages as message}
-    <MessageBox {message} />
-  {/each}
+  {#if byPeer}
+    <div class="container">
+      {#each Object.keys(groupedMessages) as peer}
+        <div class="column">
+          <h2>{peer}</h2>
+          {#each groupedMessages[peer] as message}
+            <MessageBox {message} />
+          {/each}
+        </div>
+      {/each}
+    </div>
+  {:else}
+    {#each filteredMessages as message}
+      <MessageBox {message} />
+    {/each}
+  {/if}
 </div>
 
 <style>
   .url {
-    display: inline-block;
+    display: flex;
+    align-items: center;
     margin-left: 25px;
     padding: 10px;
     font-size: 0.7rem;
+  }
+  .container {
+    display: flex;
+    justify-content: space-around;
+    max-width: 100%;
+    overflow-x: hidden;
+  }
+
+  .column {
+    flex: 1;
+  }
+
+  .check {
+    margin-left: 10px;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 10px;
+    font-size: 0.7rem;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  .column {
+    flex: 1;
+    min-width: 0;
+  }
+
+  .checkbox-label {
+    min-width: 100px; /* Adjust as needed */
   }
 </style>
