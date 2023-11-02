973e54bfcb38001d86d71e7ce44e1dc6d79006b1
ordishs <71426+ordishs@users.noreply.github.com>
2023-11-02 13:27:58 +0000

    Fix native

---
+++ cmd/propagation_blaster/main_native.go
@@ -0,0 +1,25 @@
+//go:build native
+
+package main
+
+/*
+	#cgo CFLAGS: -I./include
+	#cgo LDFLAGS: -lsecp256k1
+	#include <stdlib.h>
+	#include <secp256k1.h>
+*/
+import "C"
+import (
+	"log"
+
+	"github.com/bitcoin-sv/ubsv/native"
+	"github.com/libsv/go-bt/v2/unlocker"
+	"github.com/ordishs/gocore"
+)
+
+func init() {
+	if gocore.Config().GetBool("use_cgo_signer", false) {
+		log.Println("Using CGO signer - SignMessage")
+		unlocker.InjectExternalSignerFn(native.SignMessage)
+	}
+}
