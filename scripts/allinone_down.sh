9f803a87fcc8ef5af9795e6f5feb1fa2db996a3a
teracoder <199657068+teracoder@users.noreply.github.com>
2023-11-24 10:18:57 +0000

    [Fix] remove miner 3 6 9

--- scripts/allinone_down.sh
+++ scripts/allinone_down.sh
@@ -5,15 +5,12 @@ CONTEXT=$(kubectl config current-context)
 kubectl config use-context arn:aws:eks:eu-west-1:434394763103:cluster/aws-ubsv-playground
 kubectl scale deployment -n miner1 miner1 --replicas 0
 kubectl scale deployment -n miner2 miner2 --replicas 0
-kubectl scale deployment -n miner3 miner3 --replicas 0
 kubectl config use-context arn:aws:eks:us-east-1:434394763103:cluster/aws-ubsv-playground
 kubectl scale deployment -n miner4 miner4 --replicas 0
 kubectl scale deployment -n miner5 miner5 --replicas 0
-kubectl scale deployment -n miner6 miner6 --replicas 0
 kubectl config use-context arn:aws:eks:ap-northeast-1:434394763103:cluster/aws-ubsv-playground
 kubectl scale deployment -n miner7 miner7 --replicas 0
 kubectl scale deployment -n miner8 miner8 --replicas 0
-kubectl scale deployment -n miner9 miner9 --replicas 0
 
 # Return to the starting context
 kubectl config use-context $CONTEXT
--- scripts/allinone_up.sh
+++ scripts/allinone_up.sh
@@ -5,15 +5,12 @@ CONTEXT=$(kubectl config current-context)
 kubectl config use-context arn:aws:eks:eu-west-1:434394763103:cluster/aws-ubsv-playground
 kubectl scale deployment -n miner1 miner1 --replicas 1
 kubectl scale deployment -n miner2 miner2 --replicas 1
-kubectl scale deployment -n miner3 miner3 --replicas 1
 kubectl config use-context arn:aws:eks:us-east-1:434394763103:cluster/aws-ubsv-playground
 kubectl scale deployment -n miner4 miner4 --replicas 1
 kubectl scale deployment -n miner5 miner5 --replicas 1
-kubectl scale deployment -n miner6 miner6 --replicas 1
 kubectl config use-context arn:aws:eks:ap-northeast-1:434394763103:cluster/aws-ubsv-playground
 kubectl scale deployment -n miner7 miner7 --replicas 1
 kubectl scale deployment -n miner8 miner8 --replicas 1
-kubectl scale deployment -n miner9 miner9 --replicas 1
 
 # Return to the starting context
 kubectl config use-context $CONTEXT
@@ -21,12 +18,9 @@ kubectl config use-context $CONTEXT
 #kubectl config use-context arn:aws:eks:eu-west-1:434394763103:cluster/aws-ubsv-playground
 #kubectl scale deployment -n miner1 tx-blaster1 --replicas 1
 #kubectl scale deployment -n miner2 tx-blaster2 --replicas 1
-#kubectl scale deployment -n miner3 tx-blaster3 --replicas 1
 #kubectl config use-context arn:aws:eks:us-east-1:434394763103:cluster/aws-ubsv-playground
 #kubectl scale deployment -n miner4 tx-blaster4 --replicas 1
 #kubectl scale deployment -n miner5 tx-blaster5 --replicas 1
-#kubectl scale deployment -n miner6 tx-blaster6 --replicas 1
 #kubectl config use-context arn:aws:eks:ap-northeast-1:434394763103:cluster/aws-ubsv-playground
 #kubectl scale deployment -n miner7 tx-blaster7 --replicas 1
 #kubectl scale deployment -n miner8 tx-blaster8 --replicas 1
-#kubectl scale deployment -n miner9 tx-blaster9 --replicas 1
