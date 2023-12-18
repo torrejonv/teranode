19abff833146e1d904646025fde77f7fd7094a2e
Siggi <4411176+icellan@users.noreply.github.com>
2023-12-18 13:07:29 +0100

    Added support for multiple subtree ttl dirs

--- stores/blob/file/file_test.go
+++ stores/blob/file/file_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/bitcoin-sv/ubsv/ulogger"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -23,5 +24,71 @@ func TestFile_Get(t *testing.T) {
 
 		err = f.Del(context.Background(), []byte("key"))
 		require.NoError(t, err)
+	})
+}
+
+func TestFile_filename(t *testing.T) {
+	t.Run("1 path", func(t *testing.T) {
+		f, err := New(ulogger.TestLogger{}, "/tmp/ubsv-tests")
+		require.NoError(t, err)
+
+		filename := f.filename([]byte("key"))
+		assert.Equal(t, "/tmp/ubsv-tests/79656b", filename)
+	})
+
+	t.Run("1 paths", func(t *testing.T) {
+		f, err := New(ulogger.TestLogger{}, "/tmp/ubsv-tests", []string{"/tmp/ubsv-tests1"})
+		require.NoError(t, err)
+
+		filename := f.filename([]byte("1key"))
+		assert.Equal(t, "/tmp/ubsv-tests1/79656b31", filename)
+
+		filename = f.filename([]byte("2key"))
+		assert.Equal(t, "/tmp/ubsv-tests1/79656b32", filename)
+
+		filename = f.filename([]byte("3key"))
+		assert.Equal(t, "/tmp/ubsv-tests1/79656b33", filename)
+
+		filename = f.filename([]byte("4key"))
+		assert.Equal(t, "/tmp/ubsv-tests1/79656b34", filename)
+	})
+
+	t.Run("2 paths", func(t *testing.T) {
+		f, err := New(ulogger.TestLogger{}, "/tmp/ubsv-tests", []string{"/tmp/ubsv-tests1", "/tmp/ubsv-tests2"})
+		require.NoError(t, err)
+
+		filename := f.filename([]byte("1key"))
+		assert.Equal(t, "/tmp/ubsv-tests2/79656b31", filename)
+
+		filename = f.filename([]byte("2key"))
+		assert.Equal(t, "/tmp/ubsv-tests1/79656b32", filename)
+
+		filename = f.filename([]byte("3key"))
+		assert.Equal(t, "/tmp/ubsv-tests2/79656b33", filename)
+
+		filename = f.filename([]byte("4key"))
+		assert.Equal(t, "/tmp/ubsv-tests1/79656b34", filename)
+	})
+
+	t.Run("4 paths", func(t *testing.T) {
+		f, err := New(ulogger.TestLogger{}, "/tmp/ubsv-tests", []string{
+			"/tmp/ubsv-tests1",
+			"/tmp/ubsv-tests2",
+			"/tmp/ubsv-tests3",
+			"/tmp/ubsv-tests4",
+		})
+		require.NoError(t, err)
+
+		filename := f.filename([]byte("1key"))
+		assert.Equal(t, "/tmp/ubsv-tests2/79656b31", filename)
+
+		filename = f.filename([]byte("2key"))
+		assert.Equal(t, "/tmp/ubsv-tests3/79656b32", filename)
+
+		filename = f.filename([]byte("3key"))
+		assert.Equal(t, "/tmp/ubsv-tests4/79656b33", filename)
+
+		filename = f.filename([]byte("4key"))
+		assert.Equal(t, "/tmp/ubsv-tests1/79656b34", filename)
 	})
 }
