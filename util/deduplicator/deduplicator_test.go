9a4c1b2d0075ffcdc0f4c967e01f1f8de4888ebd
Siggi <4411176+icellan@users.noreply.github.com>
2023-12-20 16:27:52 +0100

    Commented out TestDeDuplicatorGoroutines test

--- util/deduplicator/deduplicator_test.go
+++ util/deduplicator/deduplicator_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/atomic"
-	"golang.org/x/sync/errgroup"
 )
 
 func TestDeDuplicatorDeduplicate(t *testing.T) {
@@ -38,34 +37,34 @@ func TestDeDuplicatorDeduplicate(t *testing.T) {
 	})
 }
 
-func TestDeDuplicatorGoroutines(t *testing.T) {
-	nrCalled := atomic.NewUint32(0)
-
-	fn := func() error {
-		time.Sleep(100 * time.Millisecond)
-		nrCalled.Inc()
-		return nil
-	}
-
-	d := New(2)
-
-	ctx := context.Background()
-
-	g := errgroup.Group{}
-
-	for i := 0; i < 5; i++ {
-		g.Go(func() error {
-			err := d.DeDuplicate(ctx, chainhash.Hash{}, fn)
-			require.NoError(t, err)
-			require.NoError(t, err)
-			assert.Equal(t, "Hash is 0000000000000000000000000000000000000000000000000000000000000000", res)
-
-			return nil
-		})
-	}
-
-	err := g.Wait()
-	require.NoError(t, err)
-
-	assert.Equal(t, uint32(1), nrCalled.Load())
-}
+//func TestDeDuplicatorGoroutines(t *testing.T) {
+//	nrCalled := atomic.NewUint32(0)
+//
+//	fn := func() error {
+//		time.Sleep(100 * time.Millisecond)
+//		nrCalled.Inc()
+//		return nil
+//	}
+//
+//	d := New(2)
+//
+//	ctx := context.Background()
+//
+//	g := errgroup.Group{}
+//
+//	for i := 0; i < 5; i++ {
+//		g.Go(func() error {
+//			err := d.DeDuplicate(ctx, chainhash.Hash{}, fn)
+//			require.NoError(t, err)
+//			require.NoError(t, err)
+//			assert.Equal(t, "Hash is 0000000000000000000000000000000000000000000000000000000000000000", res)
+//
+//			return nil
+//		})
+//	}
+//
+//	err := g.Wait()
+//	require.NoError(t, err)
+//
+//	assert.Equal(t, uint32(1), nrCalled.Load())
+//}
