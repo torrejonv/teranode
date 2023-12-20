53461b902f687dddc25646415c050b30e635c9f2
ordishs <71426+ordishs@users.noreply.github.com>
2023-12-20 16:00:43 +0100

    Remove AssetFetcher

--- util/deduplicator/deduplicator_test.go
+++ util/deduplicator/deduplicator_test.go
@@ -9,9 +9,10 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/atomic"
+	"golang.org/x/sync/errgroup"
 )
 
-func TestDeDuplicator_DeDuplicate(t *testing.T) {
+func TestDeDuplicatorDeduplicate(t *testing.T) {
 	t.Run("test", func(t *testing.T) {
 		nrCalled := atomic.NewUint32(0)
 
@@ -35,4 +36,36 @@ func TestDeDuplicator_DeDuplicate(t *testing.T) {
 
 		assert.Equal(t, uint32(1), nrCalled.Load())
 	})
+}
+
+func TestDeDuplicatorGoroutines(t *testing.T) {
+	nrCalled := atomic.NewUint32(0)
+
+	fn := func() error {
+		time.Sleep(100 * time.Millisecond)
+		nrCalled.Inc()
+		return nil
+	}
+
+	d := New(2)
+
+	ctx := context.Background()
+
+	g := errgroup.Group{}
+
+	for i := 0; i < 5; i++ {
+		g.Go(func() error {
+			err := d.DeDuplicate(ctx, chainhash.Hash{}, fn)
+			require.NoError(t, err)
+			require.NoError(t, err)
+			assert.Equal(t, "Hash is 0000000000000000000000000000000000000000000000000000000000000000", res)
+
+			return nil
+		})
+	}
+
+	err := g.Wait()
+	require.NoError(t, err)
+
+	assert.Equal(t, uint32(1), nrCalled.Load())
 }
