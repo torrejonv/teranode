b9071dd355135ccfe1f8430ed42ecef78a5531ec
freemans13 <freemans13@users.noreply.github.com>
2024-01-05 11:10:57 +0000

    error message wording

--- services/miner/miner.go
+++ services/miner/miner.go
@@ -113,7 +113,7 @@ func (m *Miner) Start(ctx context.Context) error {
 				err := m.mine(ctx, m.waitSeconds)
 				if err != nil {
 					if errors.Is(err, context.Canceled) {
-						m.logger.Infof("[Miner]: timeout waiting for new candidate")
+						m.logger.Infof("[Miner]: stopped waiting for new candidate (will start over)")
 					} else {
 						m.logger.Errorf("[Miner]: %v", err)
 					}
