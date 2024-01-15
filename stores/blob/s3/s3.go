3349590a2d3ec47175f4af5576735a2f7b5c9063
freemans13 <freemans13@users.noreply.github.com>
2024-01-15 18:14:19 +0000

    update to aws sdk v2 for s3

--- stores/blob/s3/s3.go
+++ stores/blob/s3/s3.go
@@ -3,8 +3,8 @@ package s3
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"io"
 	"net"
 	"net/http"
@@ -13,11 +13,12 @@ import (
 	"strings"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/bitcoin-sv/ubsv/stores/blob/options"
 	"github.com/bitcoin-sv/ubsv/tracing"
 	"github.com/bitcoin-sv/ubsv/ulogger"
@@ -27,9 +28,9 @@ import (
 )
 
 type S3 struct {
-	client     *s3.S3
-	uploader   *s3manager.Uploader
-	downloader *s3manager.Downloader
+	client     *s3.Client
+	uploader   *manager.Uploader
+	downloader *manager.Downloader
 	bucket     string
 	prefixDir  int
 	logger     ulogger.Logger
@@ -42,28 +43,26 @@ var (
 func New(logger ulogger.Logger, s3URL *url.URL, opts ...options.Options) (*S3, error) {
 	logger = logger.New("s3")
 
-	scheme := getQueryParamString(s3URL, "scheme", "http")
-	s3ForcePathStyle := getQueryParamBool(s3URL, "S3ForcePathStyle", "false")
+	// scheme := getQueryParamString(s3URL, "scheme", "http")
+	// s3ForcePathStyle := getQueryParamBool(s3URL, "S3ForcePathStyle", "false")
 	maxIdleConns := getQueryParamInt(s3URL, "MaxIdleConns", 100)
 	maxIdleConnsPerHost := getQueryParamInt(s3URL, "MaxIdleConnsPerHost", 100)
 	idleConnTimeout := time.Duration(getQueryParamInt(s3URL, "IdleConnTimeoutSeconds", 100)) * time.Second
 	timeout := time.Duration(getQueryParamInt(s3URL, "TimeoutSeconds", 30)) * time.Second
 	keepAlive := time.Duration(getQueryParamInt(s3URL, "KeepAliveSeconds", 300)) * time.Second
+	// region := getQueryParamString(s3URL, "region", "eu-west-1")
 
-	// connect to aws s3 server
-	config := &aws.Config{
-		Region:      aws.String(s3URL.Query().Get("region")),
-		Credentials: credentials.NewEnvCredentials(),
-	}
+	config, _ := config.LoadDefaultConfig(context.Background())
+	client := s3.NewFromConfig(config)
 
-	if s3URL.Host != "" {
-		serverURL := url.URL{
-			Scheme: scheme,
-			Host:   s3URL.Host,
-		}
-		config.Endpoint = aws.String(serverURL.String())
-		config.S3ForcePathStyle = aws.Bool(s3ForcePathStyle) // Required when using a non-AWS S3 service
-	}
+	// if s3URL.Host != "" {
+	// 	serverURL := url.URL{
+	// 		Scheme: scheme,
+	// 		Host:   s3URL.Host,
+	// 	}
+	// 	config.Endpoint = aws.String(serverURL.String())
+	// 	config.S3ForcePathStyle = aws.Bool(s3ForcePathStyle) // Required when using a non-AWS S3 service
+	// }
 
 	config.HTTPClient = &http.Client{
 		Transport: &http.Transport{
@@ -76,25 +75,25 @@ func New(logger ulogger.Logger, s3URL *url.URL, opts ...options.Options) (*S3, e
 			}).DialContext},
 	}
 
-	sess, err := session.NewSession(config)
-	if err != nil {
-		return nil, err
-	}
+	// sess, err := session.NewSession(config)
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	client := s3.New(sess)
+	// client := s3.New(sess)
 
 	// Setup the S3 Upload Manager. Also see the SDK doc for the Upload Manager
 	// for more information on configuring part size, and concurrency.
 	//
 	// http://docs.aws.amazon.com/sdk-for-go/api/service/s3/s3manager/#NewUploader
-	uploader := s3manager.NewUploader(sess)
+	// uploader := s3manager.NewUploader(sess)
 
-	downloader := s3manager.NewDownloader(sess)
+	// downloader := s3manager.NewDownloader(sess)
 
 	s := &S3{
 		client:     client,
-		uploader:   uploader,
-		downloader: downloader,
+		uploader:   manager.NewUploader(client),
+		downloader: manager.NewDownloader(client),
 		bucket:     s3URL.Path[1:], // remove leading slash
 		logger:     logger,
 	}
@@ -138,7 +137,7 @@ func (g *S3) SetFromReader(ctx context.Context, key []byte, reader io.ReadCloser
 
 	objectKey := g.getObjectKey(key)
 
-	uploadInput := &s3manager.UploadInput{
+	uploadInput := &s3.PutObjectInput{
 		Bucket: aws.String(g.bucket),
 		Key:    objectKey,
 		Body:   reader,
@@ -150,7 +149,7 @@ func (g *S3) SetFromReader(ctx context.Context, key []byte, reader io.ReadCloser
 		uploadInput.Expires = &expires
 	}
 
-	_, err := g.uploader.UploadWithContext(traceSpan.Ctx, uploadInput)
+	_, err := g.uploader.Upload(traceSpan.Ctx, uploadInput)
 	if err != nil {
 		traceSpan.RecordError(err)
 		return fmt.Errorf("failed to set data from reader: %w", err)
@@ -170,7 +169,7 @@ func (g *S3) Set(ctx context.Context, key []byte, value []byte, opts ...options.
 	objectKey := g.getObjectKey(key)
 
 	buf := bytes.NewBuffer(value)
-	uploadInput := &s3manager.UploadInput{
+	uploadInput := &s3.PutObjectInput{
 		Bucket: aws.String(g.bucket),
 		Key:    objectKey,
 		Body:   buf,
@@ -184,7 +183,7 @@ func (g *S3) Set(ctx context.Context, key []byte, value []byte, opts ...options.
 		uploadInput.Expires = &expires
 	}
 
-	_, err := g.uploader.UploadWithContext(traceSpan.Ctx, uploadInput)
+	_, err := g.uploader.Upload(traceSpan.Ctx, uploadInput)
 	if err != nil {
 		traceSpan.RecordError(err)
 		return fmt.Errorf("failed to set data: %w", err)
@@ -220,7 +219,7 @@ func (g *S3) GetIoReader(ctx context.Context, key []byte) (io.ReadCloser, error)
 	// We log this, since this should not happen in a healthy system. Subtrees should be retrieved from the local ttl cache
 	g.logger.Warnf("[S3][%s] Getting object reader from S3: %s", utils.ReverseAndHexEncodeSlice(key), *objectKey)
 
-	result, err := g.client.GetObjectWithContext(traceSpan.Ctx, &s3.GetObjectInput{
+	result, err := g.client.GetObject(traceSpan.Ctx, &s3.GetObjectInput{
 		Bucket: aws.String(g.bucket),
 		Key:    objectKey,
 	})
@@ -252,8 +251,8 @@ func (g *S3) Get(ctx context.Context, hash []byte) ([]byte, error) {
 		return cached, nil
 	}
 
-	buf := aws.NewWriteAtBuffer([]byte{})
-	_, err := g.downloader.DownloadWithContext(traceSpan.Ctx, buf,
+	buf := manager.NewWriteAtBuffer([]byte{})
+	_, err := g.downloader.Download(traceSpan.Ctx, buf,
 		&s3.GetObjectInput{
 			Bucket: aws.String(g.bucket),
 			Key:    objectKey,
@@ -282,18 +281,19 @@ func (g *S3) Exists(ctx context.Context, hash []byte) (bool, error) {
 		return true, nil
 	}
 
-	_, err := g.client.HeadObjectWithContext(traceSpan.Ctx, &s3.HeadObjectInput{
+	_, err := g.client.HeadObject(traceSpan.Ctx, &s3.HeadObjectInput{
 		Bucket: aws.String(g.bucket),
 		Key:    objectKey,
 	})
 	if err != nil {
 		// there was a bug in the s3 library
-		// https://github.com/aws/aws-sdk-go-v2/issues/2084
+		// https://github.com/aws/aws-sdk-go-v2-v2/issues/2084
 		if strings.Contains(err.Error(), "NotFound") {
 			return false, nil
 		}
 
-		if aerr, ok := err.(awserr.Error); ok && aerr.Code() == s3.ErrCodeNoSuchKey {
+		var noSuchKey *types.NoSuchKey
+		if errors.As(err, &noSuchKey) {
 			return false, nil
 		}
 
@@ -316,7 +316,7 @@ func (g *S3) Del(ctx context.Context, hash []byte) error {
 
 	cache.Delete(*objectKey)
 
-	_, err := g.client.DeleteObjectWithContext(traceSpan.Ctx, &s3.DeleteObjectInput{
+	_, err := g.client.DeleteObject(traceSpan.Ctx, &s3.DeleteObjectInput{
 		Bucket: aws.String(g.bucket),
 		Key:    objectKey,
 	})
@@ -325,14 +325,14 @@ func (g *S3) Del(ctx context.Context, hash []byte) error {
 		return fmt.Errorf("unable to del data: %w", err)
 	}
 
-	err = g.client.WaitUntilObjectNotExistsWithContext(traceSpan.Ctx, &s3.HeadObjectInput{
-		Bucket: aws.String(g.bucket),
-		Key:    objectKey,
-	})
-	if err != nil {
-		traceSpan.RecordError(err)
-		return fmt.Errorf("failed to del data: %w", err)
-	}
+	// err = g.client.WaitUntilObjectNotExists(traceSpan.Ctx, &s3.HeadObjectInput{
+	// 	Bucket: aws.String(g.bucket),
+	// 	Key:    objectKey,
+	// })
+	// if err != nil {
+	// 	traceSpan.RecordError(err)
+	// 	return fmt.Errorf("failed to del data: %w", err)
+	// }
 
 	return nil
 }
