8d81d295198390686173334be1553550ab7da860
cge-taal <107926963+cge-taal@users.noreply.github.com>
2024-01-26 09:57:57 +0100

    Upgrade dependencies

--- ui/dashboard/vite.config.ts
+++ ui/dashboard/vite.config.ts
@@ -1,33 +1,13 @@
-import { sentrySvelteKit } from '@sentry/sveltekit'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig, type UserConfigExport } from 'vite'
 
-// See:
-// - https://github.com/getsentry/sentry-javascript/tree/master/packages/sveltekit
-// - https://github.com/getsentry/sentry-javascript/tree/master/packages/sveltekit#uploading-source-maps
 export default defineConfig({
   build: {
     sourcemap: true,
     minify: false,
   },
   outDir: '../dist',
-  plugins: [
-    sentrySvelteKit({
-      sourceMapsUploadOptions: {
-        org: 'masagi-limited-cd21c228f',
-        project: 'javascript-sveltekit',
-      },
-    }),
-    sveltekit(),
-  ],
-  // resolve: {
-  //   alias: {
-  //     '@components': '/src/lib/components',
-  //     '@stores': '/src/stores',
-  //     '@routes': '/src/routes',
-  //     '@utils': '/src/utils',
-  //   },
-  // },
+  plugins: [sveltekit()],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
   },
