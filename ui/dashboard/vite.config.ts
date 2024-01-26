ff7624fc2111c26529e17928abf9d50ed76b544c
cge-taal <107926963+cge-taal@users.noreply.github.com>
2024-01-26 13:59:56 +0100

    Add rollup-plugin-visualizer for more stats

--- ui/dashboard/vite.config.ts
+++ ui/dashboard/vite.config.ts
@@ -1,13 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig, type UserConfigExport } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
   build: {
     sourcemap: true,
-    minify: false,
+    minify: true,
   },
   outDir: '../dist',
-  plugins: [sveltekit()],
+  plugins: [
+    sveltekit(),
+    visualizer({
+      emitFile: true,
+      filename: 'stats.html',
+    }),
+  ],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
   },
