e51c8c3e0a875cd12395b838305dff703a8a1988
cge-taal <107926963+cge-taal@users.noreply.github.com>
2024-01-29 18:25:32 +0100

    Load graph card dynamically

--- ui/dashboard/src/routes/home/+page.svelte
+++ ui/dashboard/src/routes/home/+page.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
   import HomeStatsCard from '$internal/components/page/home/home-stats-card/index.svelte'
-  import HomeStatsGraph from '$internal/components/page/home/home-stats-graph/index.svelte'
   import PageWithMenu from '$internal/components/page/template/menu/index.svelte'
+
+  let Graph
+  onMount(() => {
+    const timeoutId = setTimeout(async () => {
+      Graph = (await import('$internal/components/page/home/home-stats-graph/index.svelte')).default
+    }, 10)
+
+    return () => clearTimeout(timeoutId)
+  })
 </script>
 
 <PageWithMenu>
   <div class="content">
     <HomeStatsCard />
-    <HomeStatsGraph />
+    {#if Graph}
+      <Graph />
+    {/if}
   </div>
 </PageWithMenu>
 
