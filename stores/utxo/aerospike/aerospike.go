205861dd7e0c70da02769a213dad1e3544504700
ordishs <71426+ordishs@users.noreply.github.com>
2024-01-31 23:25:06 +0000

    Fixed utxo aerospike_server_test. ****WARNING**** This required removing a temporary "fix" that was added in the aerospike store.  Also, the batch error ALREADY_EXISTS returns an error like it did before.

--- stores/utxo/aerospike/aerospike.go
+++ stores/utxo/aerospike/aerospike.go
@@ -320,11 +320,11 @@ func (s *Store) Get(_ context.Context, spend *utxostore.Spend) (*utxostore.Respo
 		if errors.Is(aErr, aerospike.ErrKeyNotFound) {
 			return &utxostore.Response{
 				Status: int(utxostore.Status_NOT_FOUND),
-			}, utxostore.ErrNotFound
+			}, fmt.Errorf("%v: %w", aErr, utxostore.ErrNotFound)
 		}
 
 		s.logger.Errorf("Failed to get aerospike key (time taken: %s) : %v\n", time.Since(start).String(), aErr)
-		return nil, aErr
+		return nil, fmt.Errorf("%v: %w", aErr, utxostore.ErrNotFound)
 	}
 
 	var err error
@@ -428,6 +428,9 @@ func (s *Store) Store(_ context.Context, tx *bt.Tx, lockTime ...uint32) error {
 			// TODO check if this is the correct handling of this
 			// we assume because it exists, it is OK
 			if errors.As(err, &aErr) && aErr != nil && aErr.ResultCode == types.KEY_EXISTS_ERROR {
+				prometheusUtxoErrors.WithLabelValues("Store", err.Error()).Inc()
+				e := fmt.Errorf("[BATCH_ERR][%d] error in aerospike store batch record: %s - %w", batchId, utxoHashes[idx].String(), err)
+				errorsThrown = append(errorsThrown, e)
 				continue
 			}
 
@@ -538,20 +541,20 @@ func (s *Store) Spend(ctx context.Context, spends []*utxostore.Spend) (err error
 			}
 			return fmt.Errorf("context cancelled spending %d of %d aerospike utxos", i, len(spends))
 		default:
+			// if err = s.spendUtxo(policy, spend); err != nil {
+			// 	// TODO remove this hack
+			// 	// TEMP TEMP TEMP - we need to figure out why utxos are not stored properly
+			// 	// there are no double spends, so we can just ignore this error for now to be able to test performance
+			// 	s.logger.Warnf("[BACKUP_UTXO_STORE] failed to spend utxo %s on tx %s:%d: %v", spend.Hash.String(), spend.TxID.String(), spend.Vout, err)
+			// 	if err = s.storeUtxo(util.GetAerospikeWritePolicy(0, 0), spend.Hash, 0); err != nil {
+			// 		s.logger.Errorf("[BACKUP_UTXO_STORE] failed to store utxo as backup in spendUtxo %s on tx %s:%d: %v", spend.Hash.String(), spend.TxID.String(), spend.Vout, err)
+			// 	} else {
 			if err = s.spendUtxo(policy, spend); err != nil {
-				// TODO remove this hack
-				// TEMP TEMP TEMP - we need to figure out why utxos are not stored properly
-				// there are no double spends, so we can just ignore this error for now to be able to test performance
-				s.logger.Warnf("[BACKUP_UTXO_STORE] failed to spend utxo %s on tx %s:%d: %v", spend.Hash.String(), spend.TxID.String(), spend.Vout, err)
-				if err = s.storeUtxo(util.GetAerospikeWritePolicy(0, 0), spend.Hash, 0); err != nil {
-					s.logger.Errorf("[BACKUP_UTXO_STORE] failed to store utxo as backup in spendUtxo %s on tx %s:%d: %v", spend.Hash.String(), spend.TxID.String(), spend.Vout, err)
-				} else {
-					if err = s.spendUtxo(policy, spend); err != nil {
-						_ = s.UnSpend(context.Background(), spentSpends)
-						return err
-					}
-					return nil
-				}
+				// 	_ = s.UnSpend(context.Background(), spentSpends)
+				// 	return err
+				// }
+				// return nil
+				// }
 
 				// revert the spent utxos
 				_ = s.UnSpend(context.Background(), spentSpends)
