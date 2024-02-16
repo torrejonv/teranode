7b273a1fc085975d191e42058dfad9ca84c10ac3
ordishs <71426+ordishs@users.noreply.github.com>
2024-02-16 08:39:58 +0100

    Script to show m1 and m2 bestblock

---
+++ scripts/bestblock.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+function get_block_header() {
+  local node=$1
+  local url="https://$node.scaling.ubsv.dev/bestblockheader/json"
+  curl -s "$url" | jq -r '. | "\(.height): \(.hash)"' > "$2" # Write output to temp file
+}
+
+while true; do
+  tmp1=$(mktemp)
+  tmp2=$(mktemp)
+
+  get_block_header m1 "$tmp1" &
+  pid1=$!
+  get_block_header m2 "$tmp2" &
+  pid2=$!
+
+  # Wait for both background processes to finish
+  wait $pid1
+  wait $pid2
+
+  # Clear the screen and display results
+  echo -ne "\033c"
+  echo $(date -u)
+  echo ""
+  echo "m1: $(cat $tmp1)"
+  echo "m2: $(cat $tmp2)"
+  echo ""
+
+  # Clean up temporary files
+  rm "$tmp1" "$tmp2"
+
+  # Countdown before the next update
+  for (( i=10; i>0; i-- )); do
+    echo -ne "  Refreshing in $i seconds  \r"
+    sleep 1
+  done
+  echo -ne "  Refreshing...                  \r"
+done
+
