e37f783de61845f67a4463ae1cac31810a013d04
gokutheengineer <118421317+gokutheengineer@users.noreply.github.com>
2024-02-19 16:59:33 +0300

    test: fix

--- model/Block_test.go
+++ model/Block_test.go
@@ -5,14 +5,10 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"fmt"
-	"io"
-	"os"
-	"path/filepath"
 	"testing"
 	"time"
 
 	"github.com/bitcoin-sv/ubsv/stores/blob/null"
-	"github.com/bitcoin-sv/ubsv/stores/blob/options"
 	"github.com/bitcoin-sv/ubsv/stores/txmeta"
 	"github.com/bitcoin-sv/ubsv/stores/txmeta/memory"
 	"github.com/bitcoin-sv/ubsv/stores/txmetacache"
@@ -445,68 +441,4 @@ func TestCheckDuplicateTransactions(t *testing.T) {
 	_ = err // To stop lint warning
 	// TODO reactivate this test when we have a way to check for duplicate transactions
 	// require.Error(t, err)
-}
-
-type BlobStoreStub struct {
-	logger ulogger.Logger
-}
-
-func New(logger ulogger.Logger) (*BlobStoreStub, error) {
-	logger = logger.New("null")
-
-	return &BlobStoreStub{
-		logger: logger,
-	}, nil
-}
-
-func (n *BlobStoreStub) Health(_ context.Context) (int, string, error) {
-	return 0, "BlobStoreStub Store", nil
-}
-
-func (n *BlobStoreStub) Close(_ context.Context) error {
-	return nil
-}
-
-func (n *BlobStoreStub) SetFromReader(_ context.Context, _ []byte, _ io.ReadCloser, _ ...options.Options) error {
-	return nil
-}
-
-func (n *BlobStoreStub) Set(_ context.Context, _ []byte, _ []byte, _ ...options.Options) error {
-	return nil
-}
-
-func (n *BlobStoreStub) SetTTL(_ context.Context, _ []byte, _ time.Duration) error {
-	return nil
-}
-
-func (n *BlobStoreStub) GetIoReader(_ context.Context, _ []byte) (io.ReadCloser, error) {
-	path := filepath.Join("testdata", "testSubtreeHex.bin")
-
-	// read the file
-	subtreeReader, err := os.Open(path)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read file: %s", err)
-	}
-
-	return subtreeReader, nil
-}
-
-func (n *BlobStoreStub) Get(_ context.Context, hash []byte) ([]byte, error) {
-	path := filepath.Join("testdata", "testSubtreeHex.bin")
-
-	// read the file
-	subtreeBytes, err := os.ReadFile(path)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read file: %s", err)
-	}
-
-	return subtreeBytes, nil
-}
-
-func (n *BlobStoreStub) Exists(_ context.Context, _ []byte) (bool, error) {
-	return false, nil
-}
-
-func (n *BlobStoreStub) Del(_ context.Context, _ []byte) error {
-	return nil
 }
--- model/model-test-util.go
+++ model/model-test-util.go
@@ -8,12 +8,14 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"sync"
 	"time"
 
 	"github.com/bitcoin-sv/ubsv/stores/blob/options"
 	"github.com/bitcoin-sv/ubsv/stores/txmeta"
 	"github.com/bitcoin-sv/ubsv/stores/txmetacache"
+	"github.com/bitcoin-sv/ubsv/ulogger"
 	"github.com/bitcoin-sv/ubsv/util"
 	"github.com/libsv/go-bt/v2"
 	"github.com/libsv/go-bt/v2/chainhash"
@@ -474,5 +476,69 @@ func (l localSubtreeStore) Del(_ context.Context, _ []byte) error {
 }
 
 func (l localSubtreeStore) Close(_ context.Context) error {
+	return nil
+}
+
+type BlobStoreStub struct {
+	logger ulogger.Logger
+}
+
+func New(logger ulogger.Logger) (*BlobStoreStub, error) {
+	logger = logger.New("null")
+
+	return &BlobStoreStub{
+		logger: logger,
+	}, nil
+}
+
+func (n *BlobStoreStub) Health(_ context.Context) (int, string, error) {
+	return 0, "BlobStoreStub Store", nil
+}
+
+func (n *BlobStoreStub) Close(_ context.Context) error {
+	return nil
+}
+
+func (n *BlobStoreStub) SetFromReader(_ context.Context, _ []byte, _ io.ReadCloser, _ ...options.Options) error {
+	return nil
+}
+
+func (n *BlobStoreStub) Set(_ context.Context, _ []byte, _ []byte, _ ...options.Options) error {
+	return nil
+}
+
+func (n *BlobStoreStub) SetTTL(_ context.Context, _ []byte, _ time.Duration) error {
+	return nil
+}
+
+func (n *BlobStoreStub) GetIoReader(_ context.Context, _ []byte) (io.ReadCloser, error) {
+	path := filepath.Join("testdata", "testSubtreeHex.bin")
+
+	// read the file
+	subtreeReader, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %s", err)
+	}
+
+	return subtreeReader, nil
+}
+
+func (n *BlobStoreStub) Get(_ context.Context, hash []byte) ([]byte, error) {
+	path := filepath.Join("testdata", "testSubtreeHex.bin")
+
+	// read the file
+	subtreeBytes, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %s", err)
+	}
+
+	return subtreeBytes, nil
+}
+
+func (n *BlobStoreStub) Exists(_ context.Context, _ []byte) (bool, error) {
+	return false, nil
+}
+
+func (n *BlobStoreStub) Del(_ context.Context, _ []byte) error {
 	return nil
 }
