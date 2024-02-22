513cbafc1743976cb0cbaf8fdfc3f1dec12030ef
freemans13 <freemans13@users.noreply.github.com>
2024-02-22 21:44:07 +0000

    Use correct error group on second loop

--- services/blockvalidation/BlockValidation.go
+++ services/blockvalidation/BlockValidation.go
@@ -865,7 +865,7 @@ func (u *BlockValidation) validateSubtreeInternal(ctx context.Context, subtreeHa
 			if txHash != nil {
 				idx := idx
 				txHash := txHash
-				g.Go(func() error {
+				missingFromCacheG.Go(func() error {
 					txMeta, err := u.txMetaStore.GetMeta(missingFromCacheGCtx, txHash)
 					if err != nil {
 						if errors.Is(err, txmeta.NewErrTxmetaNotFound(txHash)) || strings.Contains(err.Error(), "failed to get tx meta") {
