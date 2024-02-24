0375c46f172fe8efe56ffe2d9999a1a3afb1e973
Siggi <4411176+icellan@users.noreply.github.com>
2024-02-24 04:52:43 +0100

    Mark block invalid when inserting, if parent is invalid (= invalid chain)

--- stores/blockchain/sql/GetBlock.go
+++ stores/blockchain/sql/GetBlock.go
@@ -38,13 +38,13 @@ func (s *SQL) GetBlock(ctx context.Context, blockHash *chainhash.Hash) (*model.B
 
 	q := `
 		SELECT
-	   b.version
+	     b.version
 		,b.block_time
 		,b.n_bits
-	  ,b.nonce
+	    ,b.nonce
 		,b.previous_hash
 		,b.merkle_root
-	  ,b.tx_count
+	    ,b.tx_count
 		,b.size_in_bytes
 		,b.coinbase_tx
 		,b.subtree_count
