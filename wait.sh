ff409842b8c6ca379b804968f5ca00a3174a1dd5
freemans13 <freemans13@users.noreply.github.com>
2024-03-08 10:27:52 +0000

    wait script used by docker now shows the host rathe than hardcoded 'aerospike'

--- wait.sh
+++ wait.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# wait-for-aerospike.sh
+# wait-for-port.sh
 
 set -e
 
@@ -10,12 +10,12 @@ shift 3
 cmd="$@"
 
 until nc -z "$host" "$port"; do
-  >&2 echo "Aerospike is unavailable - sleeping"
+  >&2 echo "$host is unavailable - sleeping"
   sleep 1
 done
 
->&2 echo "Aerospike is up - waiting for $waitfor seconds before executing command"
+>&2 echo "$host is up - waiting for $waitfor seconds before executing command"
 sleep $waitfor
 
->&2 echo "Aerospike is up - executing command"
+>&2 echo "$host is up - executing command"
 exec $cmd
