ed1e317f30cb094ba4594f7b5945f58908e0e425
ordishs <71426+ordishs@users.noreply.github.com>
2024-03-15 17:35:16 +0100

    http handler to nudge blockpersister

--- services/blockpersister/Server.go
+++ services/blockpersister/Server.go
@@ -2,11 +2,15 @@ package blockpersister
 
 import (
 	"context"
+	"net/http"
+	"strings"
 
+	"github.com/bitcoin-sv/ubsv/services/blockchain"
 	"github.com/bitcoin-sv/ubsv/stores/blob"
 	"github.com/bitcoin-sv/ubsv/stores/txmeta"
 	"github.com/bitcoin-sv/ubsv/ulogger"
 	"github.com/bitcoin-sv/ubsv/util"
+	"github.com/libsv/go-bt/v2/chainhash"
 	"github.com/ordishs/gocore"
 )
 
@@ -46,6 +50,44 @@ func (ps *Server) Init(_ context.Context) (err error) {
 }
 
 func (ps *Server) Start(ctx context.Context) (err error) {
+	// add a http handler that allows us to request a block to be processed
+	http.Handle("/blockpersister/block/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		hashStr := strings.TrimPrefix(r.URL.Path, "/blockpersister/block/")
+
+		hash, err := chainhash.NewHashFromStr(hashStr)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = w.Write([]byte(err.Error() + "\n"))
+			return
+		}
+
+		client, err := blockchain.NewClient(ctx, ps.logger)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(err.Error() + "\n"))
+			return
+		}
+
+		block, err := client.GetBlock(ctx, hash)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(err.Error() + "\n"))
+			return
+		}
+
+		blockBytes, err := block.Bytes()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(err.Error() + "\n"))
+			return
+		}
+
+		go ps.bp.blockFinalHandler(context.Background(), nil, blockBytes)
+
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("OK"))
+	}))
+
 	kafkaURL, err, ok := gocore.Config().GetURL("kafka_blocksFinalConfig")
 	if err == nil && ok {
 		ps.logger.Infof("[BlockPersister] Starting subtree Kafka on address: %s, with %d workers", kafkaURL.String(), 1)
