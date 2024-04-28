572078b1307756067133de02cfa5001cd525b4ed
gokutheengineer <118421317+gokutheengineer@users.noreply.github.com>
2024-04-28 18:35:36 +0300

    feat: add backoff helper

---
+++ util/retry/backoff.go
@@ -0,0 +1,19 @@
+package retry
+
+import (
+	"time"
+)
+
+// BackoffAndSleep is a utility function that will sleep according to given parameters
+// It is a blocking function, and it will sleep for 2*retries+1 seconds, and then return
+// If the logger is not nil, it will log a warning message with the message and the retry message.
+// Parameters:
+// retries: The number of times the function has been retried
+// backoffMultiplier: The multiplier that will be used to calculate the backoff time
+// durationType: The type of duration that will be used to calculate the backoff time
+func BackoffAndSleep(retries int, backoffMultiplier int, durationType time.Duration) {
+	backoff := (backoffMultiplier * retries) + 1
+	backoffPeriod := time.Duration(backoff) * durationType
+	time.Sleep(backoffPeriod)
+	return
+}
