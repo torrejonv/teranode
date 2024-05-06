1daf26e0c5a8720adf57bd4ed86420eb082b38ab
Siggi <4411176+icellan@users.noreply.github.com>
2024-05-06 11:52:16 +0200

    Removed prometheus worker errors from tx blaster, upgraded to new errors

--- cmd/txblaster/worker/worker.go
+++ cmd/txblaster/worker/worker.go
@@ -3,7 +3,6 @@ package worker
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"math/rand"
 	"net"
@@ -13,6 +12,7 @@ import (
 
 	"github.com/bitcoin-sv/ubsv/util"
 
+	"github.com/bitcoin-sv/ubsv/errors"
 	"github.com/bitcoin-sv/ubsv/services/coinbase"
 	"github.com/bitcoin-sv/ubsv/services/propagation"
 	"github.com/bitcoin-sv/ubsv/ulogger"
@@ -36,7 +36,6 @@ var (
 	prometheusInternalErrors        prometheus.Counter
 	prometheusTransactionDuration   prometheus.Histogram
 	prometheusTransactionSize       prometheus.Histogram
-	prometheusWorkerErrors          *prometheus.CounterVec
 )
 
 // ContextKey type
@@ -93,16 +92,6 @@ func _initPrometheusMetrics() {
 		prometheus.HistogramOpts{
 			Name: "tx_blaster_transactions_size",
 			Help: "Size of transactions processed by the tx blaster",
-		},
-	)
-	prometheusWorkerErrors = promauto.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "tx_blaster_worker_errors",
-			Help: "Number of tx blaster worker errors",
-		},
-		[]string{
-			"function", //function raising the error
-			"error",    // error returned
 		},
 	)
 }
@@ -164,7 +153,7 @@ func NewWorker(
 
 	address, err := bscript.NewAddressFromPublicKey(privateKey.PubKey(), true)
 	if err != nil {
-		return nil, fmt.Errorf("can't create coinbase address: %v", err)
+		return nil, errors.New(errors.ERR_PROCESSING, "can't create coinbase address", err)
 	}
 
 	var rateLimiter *rate.Limiter
@@ -210,7 +199,7 @@ func (w *Worker) Init(ctx context.Context) (err error) {
 
 	tx, err := w.coinbaseClient.RequestFunds(ctx, w.address.AddressString, true)
 	if err != nil {
-		return fmt.Errorf("error getting utxo from coinbaseTracker %s: %v", w.address.AddressString, err)
+		return errors.New(errors.ERR_SERVICE_ERROR, "error getting utxo from coinbaseTracker %s", w.address.AddressString, err)
 	}
 
 	//if w.sentTxCache != nil {
@@ -224,18 +213,18 @@ func (w *Worker) Init(ctx context.Context) (err error) {
 		}
 
 		if errors.Is(err, propagation.ErrBadRequest) {
-			return fmt.Errorf("error sending funding transaction %s: %v", tx.TxIDChainHash().String(), err)
+			return errors.New(errors.ERR_SERVICE_ERROR, "error sending funding transaction %s", tx.TxIDChainHash().String(), err)
 		}
 
 		// Go through each response and check for ErrBadRequest errors
 		for _, response := range responses {
 			if errors.Is(response.Error, propagation.ErrBadRequest) {
-				return fmt.Errorf("error sending funding transaction %s: %v", tx.TxIDChainHash().String(), response.Error)
+				return errors.New(errors.ERR_SERVICE_ERROR, "error sending funding transaction %s", tx.TxIDChainHash().String(), response.Error)
 			}
 		}
 
 		if outerRetry == 2 { // Last retry
-			return fmt.Errorf("error sending funding transaction %s: %v", tx.TxIDChainHash().String(), err)
+			return errors.New(errors.ERR_SERVICE_ERROR, "error sending funding transaction %s", tx.TxIDChainHash().String(), err)
 		}
 
 		// Retry in 5 seconds
@@ -264,7 +253,7 @@ func (w *Worker) Start(ctx context.Context) (err error) {
 	defer func() {
 		prometheusWorkers.Dec()
 		if err != nil {
-			prometheusWorkerErrors.WithLabelValues("Start", err.Error()).Inc()
+			w.logger.Errorf("Worker error: %v", err)
 		}
 	}()
 
@@ -314,7 +303,7 @@ func (w *Worker) Start(ctx context.Context) (err error) {
 		case utxo = <-w.utxoChan:
 			tx, err = w.sendTransactionFromUtxo(ctx, utxo)
 			if err != nil {
-				return fmt.Errorf("error sending transaction from utxo %s:%d: %v", utxo.TxIDHash.String(), utxo.Vout, err)
+				return errors.New(errors.ERR_TX_ERROR, "error sending transaction from utxo %s:%d", utxo.TxIDHash.String(), utxo.Vout, err)
 			}
 
 			counterLoad = counter.Add(1)
@@ -324,6 +313,7 @@ func (w *Worker) Start(ctx context.Context) (err error) {
 				if ts > 0 {
 					txPs = float64(counterLoad) / ts
 				}
+				// this needs to be a printf, since we are updating the same line
 				fmt.Printf("Time for %d transactions: %.2fs (%d tx/s)\r", counterLoad, time.Since(*w.globalStartTime).Seconds(), int(txPs))
 			}
 
@@ -357,18 +347,18 @@ func (w *Worker) sendTransactionFromUtxo(ctx context.Context, utxo *bt.UTXO) (tx
 	err = tx.FromUTXOs(utxo)
 	if err != nil {
 		prometheusInvalidTransactions.Inc()
-		return nil, fmt.Errorf("error adding utxo to tx: %v", err)
+		return nil, errors.New(errors.ERR_TX_ERROR, "error adding utxo to tx", err)
 	}
 
 	err = tx.AddP2PKHOutputFromAddress(w.address.AddressString, utxo.Satoshis)
 	if err != nil {
 		prometheusInvalidTransactions.Inc()
-		return nil, fmt.Errorf("error adding output to tx: %v", err)
+		return nil, errors.New(errors.ERR_TX_ERROR, "error adding output to tx", err)
 	}
 
 	if err = tx.FillAllInputs(ctx, w.unlocker); err != nil {
 		prometheusInvalidTransactions.Inc()
-		return nil, fmt.Errorf("error filling tx inputs: %v", err)
+		return nil, errors.New(errors.ERR_TX_ERROR, "error filling tx inputs", err)
 	}
 
 	//if w.sentTxCache != nil {
@@ -400,7 +390,7 @@ func (w *Worker) sendTransactionFromUtxo(ctx context.Context, utxo *bt.UTXO) (tx
 			}
 		}
 
-		return nil, fmt.Errorf("error sending transaction #%d: %v", counter.Load(), err)
+		return nil, errors.New(errors.ERR_TX_ERROR, "error sending transaction #%d", counter.Load(), err)
 	}
 
 	return tx, nil
