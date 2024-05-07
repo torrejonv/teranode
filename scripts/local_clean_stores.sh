0fe015310c06d9023e4b6d1aa7ead3e62a405b98
freemans13 <freemans13@users.noreply.github.com>
2024-05-07 17:05:34 +0200

    #483 Postgres only implementation of spendable_utxos balance that won't slow down over time. Should run consistently over time

--- scripts/local_clean_stores.sh
+++ scripts/local_clean_stores.sh
@@ -5,7 +5,7 @@ rm -rf "$(dirname "$0")/../data"
 
 # Drop all postgres tables
 psql postgres://ubsv:ubsv@localhost:5432/ubsv -c "SET client_min_messages TO WARNING; drop table if exists state; drop table if exists utxos; drop table if exists txmeta; drop table if exists blocks;"
-psql postgres://ubsv:ubsv@localhost:5432/coinbase -c "SET client_min_messages TO WARNING; drop table if exists coinbase_utxos; drop table if exists spendable_utxos; drop table if exists blocks; drop table if exists state;"
+psql postgres://ubsv:ubsv@localhost:5432/coinbase -c "SET client_min_messages TO WARNING; drop table if exists coinbase_utxos; drop table if exists spendable_utxos_balance; drop table if exists spendable_utxos_log; drop table if exists spendable_utxos; drop table if exists blocks; drop table if exists state;"
 
 # Flush redis
 redis-cli FLUSHALL
