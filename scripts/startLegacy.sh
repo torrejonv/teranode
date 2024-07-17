3efc1325dcb4928a5c4b3c3b56932b5a65050f03
ordishs <71426+ordishs@users.noreply.github.com>
2024-07-17 10:56:56 +0200

    Start the validator

--- scripts/startLegacy.sh
+++ scripts/startLegacy.sh
@@ -2,7 +2,7 @@
 
 LOGFILE=$(date -u +%Y-%m-%dT%H:%M:%S).log
 
-logLevel=INFO startLegacy=true legacy_verifyOnly=false nohup go run . -all=0 -blockchain=1 -legacy=1 -subtreevalidation=1 -blockvalidation=1 > $LOGFILE & 2>&1
+logLevel=INFO startLegacy=true legacy_verifyOnly=false nohup go run . -all=0 -blockchain=1 -legacy=1 -subtreevalidation=1 -blockvalidation=1 -validator=1 > $LOGFILE & 2>&1
 
 rm -f current
 
