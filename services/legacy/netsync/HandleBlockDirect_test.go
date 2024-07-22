57a2a0800b3d1699eb5e6613563f03bd0871212e
ordishs <71426+ordishs@users.noreply.github.com>
2024-07-22 15:45:41 +0100

    Use ReadBlockFromFile

--- services/legacy/netsync/HandleBlockDirect_test.go
+++ services/legacy/netsync/HandleBlockDirect_test.go
@@ -1,21 +1,17 @@
 package netsync
 
 import (
-	"os"
 	"testing"
 
-	"github.com/bitcoin-sv/ubsv/services/legacy/bsvutil"
+	"github.com/bitcoin-sv/ubsv/services/legacy/testdata"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestHandleBlockDirect(t *testing.T) {
-	// Load the block
-	f, err := os.Open("../testdata/00000000000000000ad4cd15bbeaf6cb4583c93e13e311f9774194aadea87386.bin")
-	require.NoError(t, err)
-	defer f.Close()
 
-	block, err := bsvutil.NewBlockFromReader(f)
+	// Load the block
+	block, err := testdata.ReadBlockFromFile("../testdata/00000000000000000ad4cd15bbeaf6cb4583c93e13e311f9774194aadea87386.bin")
 	require.NoError(t, err)
 
 	assert.Equal(t, block.Hash().String(), "00000000000000000ad4cd15bbeaf6cb4583c93e13e311f9774194aadea87386")
