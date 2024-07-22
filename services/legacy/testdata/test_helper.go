738f1e0d2fe1944146966303f58a32da279c09a9
ordishs <71426+ordishs@users.noreply.github.com>
2024-07-22 15:50:24 +0100

    Allow both binary and hex blocks

--- services/legacy/testdata/test_helper.go
+++ services/legacy/testdata/test_helper.go
@@ -1,10 +1,21 @@
 package testdata
 
 import (
+	"encoding/hex"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/bitcoin-sv/ubsv/services/legacy/bsvutil"
 )
+
+type binReader struct {
+	r io.Reader
+}
+
+func (br *binReader) Read(p []byte) (n int, err error) {
+	return br.r.Read(p)
+}
 
 // This function helps reading the test data from the file, returns a BSV block
 func ReadBlockFromFile(filePath string) (*bsvutil.Block, error) {
@@ -14,9 +25,20 @@ func ReadBlockFromFile(filePath string) (*bsvutil.Block, error) {
 	}
 	defer file.Close()
 
-	block, err := bsvutil.NewBlockFromReader(file)
+	var reader io.Reader
+
+	if strings.HasSuffix(filePath, ".hex") {
+		// Create a hex stream reader
+		reader = hex.NewDecoder(file)
+	} else {
+		// Create a binReader that does nothing to the stream
+		reader = &binReader{r: file}
+	}
+
+	block, err := bsvutil.NewBlockFromReader(reader)
 	if err != nil {
 		return nil, err
 	}
+
 	return block, nil
 }
