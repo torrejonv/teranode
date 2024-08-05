c7db78bc3384edf93706951a6179552dcc62a01a
ordishs <71426+ordishs@users.noreply.github.com>
2024-08-06 00:22:44 +0200

    fix script

--- scripts/startLegacyNative.sh
+++ scripts/startLegacyNative.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Determine the directory of the script
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# Navigate to the script's directory (optional, if make needs to run in the script directory)
-cd "$SCRIPT_DIR" || exit 1
+cd $(dirname "$0")
 
 # Build the project
 make build-ubsv
 
 # Run startLooper.sh with nohup in the same directory
-nohup "$SCRIPT_DIR/startLooper.sh" &
+nohup ./startLooper.sh &
