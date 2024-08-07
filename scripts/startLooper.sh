7ba6a9b1e021bbaae230ad4cb74a054b6c85f514
ordishs <71426+ordishs@users.noreply.github.com>
2024-08-07 13:10:42 +0200

    start utxopersister

--- scripts/startLooper.sh
+++ scripts/startLooper.sh
@@ -5,7 +5,7 @@ while true; do
     LOGFILE=$(date -u +%Y-%m-%dT%H:%M:%S).log
     
     # Start the process and redirect both stdout and stderr to the log file
-    logLevel=INFO startLegacy=true blockassembly_disabled=true legacy_verifyOnly=false ./ubsv.run -all=0 -blockchain=1 -legacy=1 -subtreevalidation=1 -blockvalidation=1 -validator=1 -blockpersister=1 > $LOGFILE 2>&1 &
+    logLevel=INFO startLegacy=true blockassembly_disabled=true legacy_verifyOnly=false ./ubsv.run -all=0 -blockchain=1 -legacy=1 -subtreevalidation=1 -blockvalidation=1 -validator=1 -blockpersister=1 -utxopersister=1 > $LOGFILE 2>&1 &
 
     # Get the PID of the last process started in the background
     PID=$!
