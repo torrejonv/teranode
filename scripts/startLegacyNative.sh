955cead0675fdbecbbe1335cb849cd42a16813ab
ordishs <71426+ordishs@users.noreply.github.com>
2024-08-08 11:44:50 +0200

    Use one log file for restarting legacy

--- scripts/startLegacyNative.sh
+++ scripts/startLegacyNative.sh
@@ -5,5 +5,11 @@ cd $(dirname "$0")/..
 # Build the project
 make build-ubsv
 
+LOGFILE=$(date -u +%Y-%m-%dT%H:%M:%S).log
+
+# Update the symlink to the current log file
+rm -f current
+ln -s $LOGFILE current
+
 # Run startLooper.sh with nohup in the same directory
-nohup ./scripts/startLooper.sh &
+nohup ./scripts/startLooper.sh $LOGFILE >> $LOGFILE 2>&1 &
--- scripts/startLooper.sh
+++ scripts/startLooper.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+# Check if logfile argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <logfile>"
+    exit 1
+fi
+
+LOGFILE=$1
+
 # Main loop to keep the process running
 while true; do
-    LOGFILE=$(date -u +%Y-%m-%dT%H:%M:%S).log
-    
     # Start the process and redirect both stdout and stderr to the log file
-    logLevel=INFO startLegacy=true blockassembly_disabled=true legacy_verifyOnly=false ./ubsv.run -all=0 -blockchain=1 -legacy=1 -subtreevalidation=1 -blockvalidation=1 -validator=1 -blockpersister=1 -utxopersister=1 > $LOGFILE 2>&1 &
+    logLevel=INFO startLegacy=true blockassembly_disabled=true legacy_verifyOnly=false ./ubsv.run -all=0 -blockchain=1 -legacy=1 -subtreevalidation=1 -blockvalidation=1 -validator=1 -blockpersister=1 -utxopersister=1 >> $LOGFILE 2>&1 &
 
     # Get the PID of the last process started in the background
     PID=$!
-
-    # Update the symlink to the current log file
-    rm -f current
-    ln -s $LOGFILE current
 
     # Wait for the process to exit
     wait $PID
 
     # Optional: Log the restart event
-    echo "$(date -u +%Y-%m-%dT%H:%M:%S) - Process exited with status $?. Restarting..." >> $LOGFILE
+    echo "$(date -u +%Y-%m-%dT%H:%M:%S) - Process exited with status $?. Restarting..." >> $LOGFILE 2>&1
 
     # Sleep for a short period to avoid rapid restart in case of immediate failure
     sleep 10
