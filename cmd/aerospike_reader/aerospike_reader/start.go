c0e583a8105370e8811ef9f8e121d552664872a7
ordishs <71426+ordishs@users.noreply.github.com>
2024-08-12 10:55:08 +0200

    Add aerospike_reader

---
+++ cmd/aerospike_reader/aerospike_reader/start.go
@@ -0,0 +1,49 @@
+package aerospike_reader
+
+import (
+	"context"
+	"os"
+
+	"github.com/bitcoin-sv/ubsv/stores/utxo"
+	"github.com/bitcoin-sv/ubsv/stores/utxo/_factory"
+	"github.com/bitcoin-sv/ubsv/ulogger"
+	"github.com/libsv/go-bt/v2/chainhash"
+	"github.com/ordishs/gocore"
+)
+
+func Start() {
+	ctx := context.Background()
+	logger := ulogger.NewGoCoreLogger("aerospike_reader")
+
+	utxoStore := getUtxoStore(ctx, logger)
+
+	txid := os.Args[1]
+
+	hash, err := chainhash.NewHashFromStr(txid)
+	if err != nil {
+		panic(err)
+	}
+
+	tx, err := utxoStore.Get(ctx, hash)
+	if err != nil {
+		panic(err)
+	}
+
+	logger.Infof("tx: %#v", tx)
+}
+
+func getUtxoStore(ctx context.Context, logger ulogger.Logger) utxo.Store {
+	utxoStoreURL, err, found := gocore.Config().GetURL("utxostore")
+	if err != nil {
+		panic(err)
+	}
+	if !found {
+		panic("no utxostore setting found")
+	}
+	utxoStore, err := _factory.NewStore(ctx, logger, utxoStoreURL, "aerospike_reader")
+	if err != nil {
+		panic(err)
+	}
+
+	return utxoStore
+}
---
+++ cmd/aerospike_reader/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/bitcoin-sv/ubsv/cmd/aerospike_reader/aerospike_reader"
+)
+
+func main() {
+	aerospike_reader.Start()
+}
