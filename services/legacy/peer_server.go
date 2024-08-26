0ed348163916b5d11f23d65cf9e576c56a76574c
Siggi <4411176+icellan@users.noreply.github.com>
2024-08-26 18:57:15 +0200

    Force legacy to only connect to legacy_connect_peers with MaxPeers set

--- services/legacy/peer_server.go
+++ services/legacy/peer_server.go
@@ -2265,13 +2265,16 @@ func newServer(ctx context.Context, logger ulogger.Logger, config Config, blockc
 		return nil, err
 	}
 
-	// Add the peers the are defined in teranode settings...
+	// Add the peers that are defined in teranode settings...
 	addresses, found := gocore.Config().GetMulti("legacy_connect_peers", "|")
 	if found {
 		c.ConnectPeers = append(c.ConnectPeers, addresses...)
+		// set max peers to the number of connect peers
+		// this forces the server to only connect to the peers defined in the settings
+		c.MaxPeers = len(c.ConnectPeers)
 	}
 
-	// Only setup a function to return new addresses to connect to when
+	// Only set up a function to return new addresses to connect to when
 	// not running in connect-only mode.  The simulation network is always
 	// in connect-only mode since it is only intended to connect to
 	// specified peers and actively avoid advertising and connecting to
