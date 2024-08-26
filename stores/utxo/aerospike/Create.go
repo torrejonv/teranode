7a2fa6b6b8dfb2613e1ffc99a7e45c8c4425ab6b
Siggi <4411176+icellan@users.noreply.github.com>
2024-08-26 15:10:00 +0200

    Upgraded aerospike client, fixed linter, added aerospike metrics, deactivated otel (slow mutexes)

--- stores/utxo/aerospike/Create.go
+++ stores/utxo/aerospike/Create.go
@@ -5,6 +5,7 @@ package aerospike
 import (
 	"context"
 	"math"
+	"time"
 
 	"github.com/aerospike/aerospike-client-go/v7"
 	"github.com/aerospike/aerospike-client-go/v7/types"
@@ -19,7 +20,6 @@ import (
 	"github.com/libsv/go-bt/v2"
 	"github.com/libsv/go-bt/v2/chainhash"
 	"github.com/ordishs/go-utils"
-	"github.com/ordishs/gocore"
 )
 
 // Used for NOOP batch operations
@@ -95,8 +95,16 @@ func (s *Store) Create(ctx context.Context, tx *bt.Tx, blockHeight uint32, opts 
 }
 
 func (s *Store) sendStoreBatch(batch []*batchStoreItem) {
-	start := gocore.CurrentTime()
-	stat := gocore.NewStat("sendStoreBatch")
+	start := time.Now()
+	_, stat, deferFn := tracing.StartTracing(context.Background(), "sendStoreBatch",
+		tracing.WithParentStat(stat),
+		tracing.WithHistogram(prometheusUtxoCreateBatch),
+	)
+
+	defer func() {
+		prometheusUtxoCreateBatchSize.Observe(float64(len(batch)))
+		deferFn()
+	}()
 
 	batchPolicy := util.GetAerospikeBatchPolicy()
 
--- util/metrics.go
+++ util/metrics.go
@@ -16,6 +16,10 @@ var MetricsBucketsSeconds = []float64{
 	1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048,
 }
 
+var MetricsBucketsSizeSmall = []float64{
+	1, 16, 32, 64, 128, 256, 1024, 2048, 4096, 8192, 16384, 32768,
+}
+
 var MetricsBucketsSize = []float64{
 	128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144,
 }
