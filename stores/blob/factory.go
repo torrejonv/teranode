7136464d01144652a0946504b30e8894844de434
freemans13 <freemans13@users.noreply.github.com>
2024-08-28 16:16:14 +0100

    funky lint error

--- stores/blob/factory.go
+++ stores/blob/factory.go
@@ -52,14 +52,14 @@ func NewStore(logger ulogger.Logger, storeURL *url.URL, opts ...options.Options)
 	}
 
 	if storeURL.Query().Get("batch") == "true" {
-		store, err = createBatchedStore(storeURL, err, store, logger)
+		store, err = createBatchedStore(storeURL, store, logger)
 		if err != nil {
 			return nil, errors.NewStorageError("error creating batch blob store", err)
 		}
 	}
 
 	if storeURL.Query().Get("localTTLStore") != "" {
-		store, err = createTTLStore(storeURL, err, logger, opts, store)
+		store, err = createTTLStore(storeURL, logger, opts, store)
 		if err != nil {
 			return nil, errors.NewStorageError("error creating local TTL blob store", err)
 		}
@@ -68,7 +68,7 @@ func NewStore(logger ulogger.Logger, storeURL *url.URL, opts ...options.Options)
 	return
 }
 
-func createTTLStore(storeURL *url.URL, err error, logger ulogger.Logger, opts []options.Options, store Store) (Store, error) {
+func createTTLStore(storeURL *url.URL, logger ulogger.Logger, opts []options.Options, store Store) (Store, error) {
 	localTTLStorePath := storeURL.Query().Get("localTTLStorePath")
 	if localTTLStorePath == "" {
 		localTTLStorePath = "/tmp/localTTL"
@@ -81,7 +81,7 @@ func createTTLStore(storeURL *url.URL, err error, logger ulogger.Logger, opts []
 
 	var ttlStore Store
 
-	ttlStore, err = file.New(logger, localTTLStorePaths, opts...)
+	ttlStore, err := file.New(logger, localTTLStorePaths, opts...)
 	if err != nil {
 		return nil, errors.NewStorageError("failed to create file store", err)
 	}
@@ -90,14 +90,17 @@ func createTTLStore(storeURL *url.URL, err error, logger ulogger.Logger, opts []
 	if err != nil {
 		return nil, errors.NewStorageError("failed to create localTTL store", err)
 	}
+
 	return store, nil
 }
 
-func createBatchedStore(storeURL *url.URL, err error, store Store, logger ulogger.Logger) (Store, error) {
+func createBatchedStore(storeURL *url.URL, store Store, logger ulogger.Logger) (Store, error) {
 	sizeInBytes := int64(4 * 1024 * 1024)
 
 	sizeString := storeURL.Query().Get("sizeInBytes")
 	if sizeString != "" {
+		var err error
+
 		sizeInBytes, err = strconv.ParseInt(sizeString, 10, 64)
 		if err != nil {
 			return nil, errors.NewConfigurationError("error parsing batch size", err)
@@ -110,6 +113,7 @@ func createBatchedStore(storeURL *url.URL, err error, store Store, logger ulogge
 	}
 
 	store = batcher.New(logger, store, int(sizeInBytes), writeKeys)
+
 	return store, nil
 }
 
