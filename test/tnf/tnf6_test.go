ce5709512e9f50fdfad2d6be076705ebea3fdb3c
sugh01 <19183308+sugh01@users.noreply.github.com>
2024-09-11 09:42:27 +0200

    fix workflow

--- test/tnf/tnf6_test.go
+++ test/tnf/tnf6_test.go
@@ -1,9 +1,8 @@
 //go:build tnftests
 
 // How to run this test:
-// $ unzip data.zip
-// $ cd test/functional/
-// $ `SETTINGS_CONTEXT=docker.ci.tc1.run go test -run TestShouldAllowFairTx`
+// $ cd test/tnf/
+// $ SETTINGS_CONTEXT=docker.ci.tc1.run go test -v -run "^TestTNFTestSuite$/TestInvalidateBlock$" --tags tnftests
 
 package tnf
 
@@ -71,11 +70,6 @@ func (suite *TNFTestSuite) TestInvalidateBlock() {
 		t.Errorf("Failed to invalidate block: %v", err)
 	}
 
-	_, errMine0 := helper.MineBlock(ctx, cluster.Nodes[0].BlockassemblyClient, logger)
-	if errMine0 != nil {
-		t.Errorf("Failed to mine block: %v", errMine0)
-	}
-
 	header1, _, _ := cluster.Nodes[2].BlockchainClient.GetBestBlockHeader(ctx)
 	blockchain1 := cluster.Nodes[2].BlockchainClient
 	err = blockchain1.InvalidateBlock(ctx, header1.Hash())
@@ -86,8 +80,16 @@ func (suite *TNFTestSuite) TestInvalidateBlock() {
 
 	time.Sleep(20 * time.Second)
 
+	_, errMine0 := helper.MineBlock(ctx, cluster.Nodes[0].BlockassemblyClient, logger)
+	if errMine0 != nil {
+		t.Errorf("Failed to mine block: %v", errMine0)
+	}
+
 	header1, _, _ = blockchain1.GetBestBlockHeader(ctx)
 	fmt.Printf("Best block header1: %v\n", header1.Hash())
+
+	header, _, _ = blockchain.GetBestBlockHeader(ctx)
+	fmt.Printf("Best block header: %v\n", header.Hash())
 
 	assert.NotEqual(t, header.Hash(), header1.Hash(), "Blocks are equal")
 }
