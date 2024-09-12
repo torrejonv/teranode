71ea1b526b12c329493d1ad0d7b63b78704a37e2
ordishs <71426+ordishs@users.noreply.github.com>
2024-09-12 14:49:13 +0200

    Lint fixing

--- tracing/tracing.go
+++ tracing/tracing.go
@@ -99,7 +99,7 @@ func (s *TraceOptions) addLogMessage(logger ulogger.Logger, message, level strin
 }
 
 // StartTracing starts a new span with the given name and returns a context with the span and a function to finish the span.
-func StartTracing(ctx context.Context, name string, setOptions ...Options) (context.Context, *gocore.Stat, func(err ...error)) {
+func StartTracing(ctx context.Context, name string, setOptions ...Options) (context.Context, *gocore.Stat, func(...error)) {
 	span, spanCtx := opentracing.StartSpanFromContext(ctx, name)
 
 	// process the options
