66e1a95f6126bcb7648058dacba198e2f582df0b
ordishs <71426+ordishs@users.noreply.github.com>
2024-09-13 16:48:23 +0200

    Fix compile errors

--- services/subtreevalidation/subtreeHandler_test.go
+++ services/subtreevalidation/subtreeHandler_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/bitcoin-sv/ubsv/stores/blob/options"
 	"github.com/bitcoin-sv/ubsv/ulogger"
+	"github.com/bitcoin-sv/ubsv/util/quorum"
 	"github.com/libsv/go-bt/v2/chainhash"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -22,25 +23,34 @@ func (m MockExister) Exists(ctx context.Context, key []byte, opts ...options.Opt
 
 func TestLock(t *testing.T) {
 	exister := MockExister{}
+
+	quorumPath, _ := gocore.Config().Get("subtree_quorum_path", "")
+
 	defer func() {
 		// remove quorum path
-		quorumPath, _ := gocore.Config().Get("subtree_quorum_path", "")
 		if quorumPath != "" {
 			_ = os.RemoveAll(quorumPath)
 		}
 	}()
+
+	q, err := quorum.New(ulogger.TestLogger{}, exister, quorumPath)
+	require.NoError(t, err)
 
 	hash := chainhash.HashH([]byte("test"))
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	gotLock, _, _, err := tryLockIfNotExists(ctx, ulogger.TestLogger{}, exister, &hash)
+	gotLock, _, releaseFn, err := q.TryLockIfNotExists(ctx, &hash)
 	require.NoError(t, err)
 	assert.True(t, gotLock)
 
-	gotLock, _, _, err = tryLockIfNotExists(ctx, ulogger.TestLogger{}, exister, &hash)
+	defer releaseFn()
+
+	gotLock, _, releaseFn, err = q.TryLockIfNotExists(ctx, &hash)
 	require.NoError(t, err)
 	assert.False(t, gotLock)
+
+	defer releaseFn()
 
 }
