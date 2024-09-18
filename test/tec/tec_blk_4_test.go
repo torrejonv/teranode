820d0f008d36de73d86c07265d2dc100416686f9
sugh01 <19183308+sugh01@users.noreply.github.com>
2024-09-18 14:38:28 +0200

    refactor test setup (#1146)

---
+++ test/tec/tec_blk_4_test.go
@@ -0,0 +1,42 @@
+//go:build tecblk4test
+
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/bitcoin-sv/ubsv/test/setup"
+	"github.com/stretchr/testify/suite"
+)
+
+type TECBlk4TestSuite struct {
+	setup.BitcoinTestSuite
+}
+
+func (suite *TECBlk4TestSuite) InitSuite() {
+	suite.SettingsMap = map[string]string{
+		"SETTINGS_CONTEXT_1": "docker.ci.ubsv1.tec4",
+		"SETTINGS_CONTEXT_2": "docker.ci.ubsv1.tec4",
+		"SETTINGS_CONTEXT_3": "docker.ci.ubsv1.tec4",
+	}
+}
+
+func (suite *TECBlk4TestSuite) SetupTest() {
+	suite.SetupTestWithCustomComposeAndSettings(
+		suite.SettingsMap,
+		[]string{"../../docker-compose.yml", "../../docker-compose.aerospike.override.yml", "../../docker-compose.e2etest.override.yml"},
+	)
+}
+
+func (suite *TECBlk4TestSuite) TearDownTest() {
+
+}
+
+func (suite *TECBlk4TestSuite) TestBlockValidationRecoverability() {
+	fmt.Println("Setting up Teranode without Block Validation...")
+}
+
+func TestTECBlk4TestSuite(t *testing.T) {
+	suite.Run(t, new(TECBlk4TestSuite))
+}
