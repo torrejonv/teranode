273a4de55edddbcaa9a622c5c36f7e16bbcaa860
rid3thespiral <24756750+rid3thespiral@users.noreply.github.com>
2024-10-01 15:46:18 +0200

    tec5

---
+++ test/tec/tec_blk_5_test.go
@@ -0,0 +1,42 @@
+//go:build tecblk5test
+
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/bitcoin-sv/ubsv/test/setup"
+	"github.com/stretchr/testify/suite"
+)
+
+type TECBlk5TestSuite struct {
+	setup.BitcoinTestSuite
+}
+
+func (suite *TECBlk5TestSuite) InitSuite() {
+	suite.SettingsMap = map[string]string{
+		"SETTINGS_CONTEXT_1": "docker.ci.ubsv1.tec5",
+		"SETTINGS_CONTEXT_2": "docker.ci.ubsv1.tec5",
+		"SETTINGS_CONTEXT_3": "docker.ci.ubsv1.tec5",
+	}
+}
+
+func (suite *TECBlk5TestSuite) SetupTest() {
+	suite.SetupTestWithCustomComposeAndSettingsSkipChecks(
+		suite.SettingsMap,
+		[]string{"../../docker-compose.yml", "../../docker-compose.aerospike.override.yml", "../../docker-compose.e2etest.yml"},
+	)
+}
+
+func (suite *TECBlk5TestSuite) TearDownTest() {
+
+}
+
+func (suite *TECBlk5TestSuite) TestP2PRecoverability() {
+	fmt.Println("Setting up Teranode without P2P httpListenAddress...")
+}
+
+func TestTECBlk5TestSuite(t *testing.T) {
+	suite.Run(t, new(TECBlk5TestSuite))
+}
