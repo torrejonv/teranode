ae787007a8c2a832c70832435a51795a6856cfa4
gokutheengineer <118421317+gokutheengineer@users.noreply.github.com>
2024-10-09 14:28:11 +0300

    chore: fix lint

--- util/MapToSlice.go
+++ util/MapToSlice.go
@@ -4,7 +4,6 @@ import "sync"
 
 func ConvertSyncMapToUint32Slice(oldBlockIDs *sync.Map) ([]uint32, bool) {
 	var referencedOldBlockIDs []uint32
-	// Flag to check if the oldBlockIDs map has elements
 	var hasTransactionsReferencingOldBlocks bool
 
 	oldBlockIDs.Range(func(key, value interface{}) bool {
@@ -12,7 +11,7 @@ func ConvertSyncMapToUint32Slice(oldBlockIDs *sync.Map) ([]uint32, bool) {
 		val := value.(uint32)
 		referencedOldBlockIDs = append(referencedOldBlockIDs, val)
 
-		return true // Continue iteration to collect all elements
+		return true
 	})
 
 	return referencedOldBlockIDs, hasTransactionsReferencingOldBlocks
