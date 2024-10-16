c0e1c45d11b12a3432cb3516c0b0826f8b2acb3e
gokutheengineer <118421317+gokutheengineer@users.noreply.github.com>
2024-10-16 13:55:02 +0300

    feat (WIP): add get set functions and test

--- stores/blockchain/sql/GetFSMState.go
+++ stores/blockchain/sql/GetFSMState.go
@@ -2,9 +2,32 @@ package sql
 
 import (
 	"context"
-	"github.com/bitcoin-sv/ubsv/services/blockchain"
+	"database/sql"
+	"fmt"
 )
 
-func (s *SQL) SetFSMState(ctx context.Context, state blockchain.FSMStateType) (err error) {
-	return nil
+func (s *SQL) GetFSMState(ctx context.Context) (string, error) {
+	const query = `SELECT fsm_state FROM state WHERE key = $1;`
+
+	var stateStr sql.NullString
+	err := s.db.QueryRowContext(ctx, query, "fsm_state").Scan(&stateStr)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// Return default state or handle accordingly
+			return "", nil
+		}
+		return "", fmt.Errorf("failed to get FSM state: %w", err)
+	}
+
+	if !stateStr.Valid {
+		return "", fmt.Errorf("FSM state is NULL")
+	}
+
+	// Map the string back to FSMStateType
+	//fsmStateValue, ok := blockchain_api.FSMStateType_value[stateStr.String]
+	//if !ok {
+	//	return blockchain.FSMStateType(0), fmt.Errorf("invalid FSM state value: %s", stateStr.String)
+	//}
+
+	return stateStr.String, nil
 }
--- stores/blockchain/sql/SetGetFSMState_test.go
+++ stores/blockchain/sql/SetGetFSMState_test.go
@@ -1,1 +1,29 @@
 package sql
+
+import (
+	"context"
+	"fmt"
+	"github.com/bitcoin-sv/ubsv/ulogger"
+	"github.com/stretchr/testify/require"
+	"net/url"
+	"testing"
+)
+
+func Test_SetGetFSMState(t *testing.T) {
+	connStr, teardown := setupPostgresContainer(t)
+	defer teardown()
+
+	storeURL, err := url.Parse(connStr)
+	require.NoError(t, err)
+
+	s, err := New(ulogger.TestLogger{}, storeURL)
+	require.NoError(t, err)
+
+	err = s.SetFSMState(context.Background(), "CATCHING_BLOCKS")
+	require.NoError(t, err)
+
+	state, err := s.GetFSMState(context.Background())
+	fmt.Println("State: ", state)
+	require.NoError(t, err)
+	require.Equal(t, "CATCHING_BLOCKS", state)
+}
