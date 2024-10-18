1858d25aa93aa8cfdcdebf535d5c8f47a37bde22
freemans13 <freemans13@users.noreply.github.com>
2024-10-18 14:27:05 +0100

    More linty stuff - annoying me now

--- services/blockchain/server_test.go
+++ services/blockchain/server_test.go
@@ -84,23 +84,13 @@ func Test_GetBlock(t *testing.T) {
 	_, _, err := ctx.server.store.StoreBlock(context.Background(), mockBlock(ctx, t), "")
 	require.NoError(t, err)
 
-	// var wg sync.WaitGroup
-	// wg.Add(1)
-
-	// Start the server
-	// ctx := context.Background()
-	// go func() {
-	// 	//defer wg.Done()
-	// 	if err := server.Start(ctx); err != nil {
-	// 		t.Errorf("Failed to start server: %v", err)
-	// 	}
-	// }()
-
+	context := context.Background()
 	request := &blockchain_api.GetBlockRequest{
 		Hash: []byte{1},
 	}
 
-	block, err := ctx.server.GetBlock(context.Background(), request)
+	block, err := ctx.server.GetBlock(context, request)
+	require.Error(t, err)
 	require.Empty(t, block)
 
 	// TODO: Put this back in when we fix WrapGRPC/UnwrapGRPC
@@ -112,24 +102,18 @@ func Test_GetBlock(t *testing.T) {
 		Height: 1,
 	}
 
-	block, err = ctx.server.GetBlockByHeight(context.Background(), requestHeight)
+	block, err = ctx.server.GetBlockByHeight(context, requestHeight)
 	require.Error(t, err, "Expected error")
 	require.Empty(t, block, "Expected block to be empty")
-
 	// unwrap the error
 	// TODO: Put this back in when we fix WrapGRPC/UnwrapGRPC
-	// unwrappedErr = errors.UnwrapGRPC(err)
-	// require.ErrorIs(t, unwrappedErr, errors.ErrBlockNotFound)
-	// var tErr *errors.Error
-	// assert.ErrorAs(t, unwrappedErr, &tErr)
-	// assert.Equal(t, tErr.Code, errors.ERR_BLOCK_NOT_FOUND)
-	// assert.Equal(t, tErr.Message, "block not found")
+	unwrappedErr := errors.UnwrapGRPC(err)
+	require.ErrorIs(t, unwrappedErr, errors.ErrBlockNotFound)
 
-	//wg.Wait()
 	// Stop the server
-	// if err := server.Stop(ctx); err != nil {
-	// 	t.Fatalf("Failed to stop server: %v", err)
-	// }
+	if err := ctx.server.Stop(context); err != nil {
+		t.Fatalf("Failed to stop server: %v", err)
+	}
 }
 
 func Test_GetFSMCurrentState(t *testing.T) {
@@ -137,31 +121,10 @@ func Test_GetFSMCurrentState(t *testing.T) {
 	_, _, err := ctx.server.store.StoreBlock(context.Background(), mockBlock(ctx, t), "")
 	require.NoError(t, err)
 
-	// var wg sync.WaitGroup
-	// wg.Add(1)
-
-	// Assuming that the setup or initialization of the server sets the FSM state
-	// Start the server and the FSM in an initial state you control (not shown here)
-
-	//ctx := context.Background()
-	// go func() {
-	// 	defer wg.Done()
-	// if err := server.Start(ctx); err != nil {
-	// 	t.Errorf("Failed to start server: %v", err)
-	// }
-	//	}()
-
-	// Test the GetFSMCurrentState function
 	response, err := ctx.server.GetFSMCurrentState(context.Background(), &emptypb.Empty{})
 	require.NoError(t, err)
 	require.NotNil(t, response)
 	assert.Equal(t, blockchain_api.FSMStateType_STOPPED, response.State, "Expected FSM state did not match")
-
-	//wg.Wait()
-	// Stop the server
-	// if err := server.Stop(ctx); err != nil {
-	// 	t.Fatalf("Failed to stop server: %v", err)
-	// }
 }
 
 type testContext struct {
@@ -222,8 +185,8 @@ func mockBlock(ctx *testContext, t *testing.T) *model.Block {
 	blockHeader := &model.BlockHeader{
 		Version:        1,
 		HashPrevBlock:  hashPrevBlock,
-		HashMerkleRoot: subtree.RootHash(), // doesn't matter, we're only checking the value and not whether it's correct
-		Timestamp:      uint32(time.Now().Unix()),
+		HashMerkleRoot: subtree.RootHash(),        // doesn't matter, we're only checking the value and not whether it's correct
+		Timestamp:      uint32(time.Now().Unix()), // nolint:gosec
 		Bits:           *nBits,
 		Nonce:          0,
 	}
@@ -240,7 +203,6 @@ func mockBlock(ctx *testContext, t *testing.T) *model.Block {
 }
 
 var (
-	//tx, _  = hex.DecodeString("0100000001ae73759b118b9c8d54a13ad4ccd5de662bbaa2175ee7f4b413f402affb831ed3000000006b483045022100a6af846212b0c611056a9a30c22f0eed3adc29f8c688e804509b113f322459220220708fb79c66d235d937e8348ac022b3cfa6b64fec8d35a749ea0b2293ad95da014121039f271b930111fd7c818100ee1603d5c5094c68b3d15ad0a58f712e7d766225edffffffff0550c30000000000001976a91448bea2d45f4f6175e47ccb717e4f5d19d8f68f3b88ac204e0000000000001976a91442859b9bada6461d08a0aab8a18105ef30457a8b88ac10270000000000001976a914d0e2122bdeed7b2235f670cdc832f518fb63db9f88ac0c040000000000001976a914d56f84ae869e4a743e929e31218b198f02ce67fe88ac8d0c0100000000001976a91444a8e7fb1a426e4c60597d9d3f534c677d4f858388ac00000000")
 	tx1, _ = bt.NewTxFromString("010000000000000000ef0152a9231baa4e4b05dc30c8fbb7787bab5f460d4d33b039c39dd8cc006f3363e4020000006b483045022100ce3605307dd1633d3c14de4a0cf0df1439f392994e561b648897c4e540baa9ad02207af74878a7575a95c9599e9cdc7e6d73308608ee59abcd90af3ea1a5c0cca41541210275f8390df62d1e951920b623b8ef9c2a67c4d2574d408e422fb334dd1f3ee5b6ffffffff706b9600000000001976a914a32f7eaae3afd5f73a2d6009b93f91aa11d16eef88ac05404b4c00000000001976a914aabb8c2f08567e2d29e3a64f1f833eee85aaf74d88ac80841e00000000001976a914a4aff400bef2fa074169453e703c611c6b9df51588ac204e0000000000001976a9144669d92d46393c38594b2f07587f01b3e5289f6088ac204e0000000000001976a914a461497034343a91683e86b568c8945fb73aca0288ac99fe2a00000000001976a914de7850e419719258077abd37d4fcccdb0a659b9388ac00000000")
 	hash1  = tx1.TxIDChainHash()
 )
@@ -352,7 +314,7 @@ func (s *mockStore) GetBlocksByTime(ctx context.Context, fromTime, toTime time.T
 	panic("not implemented")
 }
 func (s *mockStore) LocateBlockHeaders(ctx context.Context, locator []*chainhash.Hash, hashStop *chainhash.Hash, maxHashes uint32) ([]*model.BlockHeader, error) {
-	//TODO implement me
+	// TODO implement me
 	panic("implement me")
 }
 
