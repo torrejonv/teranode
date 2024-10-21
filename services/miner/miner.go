f2627f97c98e31b1cc9efb73af38c66f6bc54340
freemans13 <freemans13@users.noreply.github.com>
2024-10-21 16:12:51 +0100

    miner - remove need for mine_initial_blocks_final_wait and added new setting mine_start_height which wait til another node has mined n blocks before starting to mine - used for testing

--- services/miner/miner.go
+++ services/miner/miner.go
@@ -34,7 +34,6 @@ type Miner struct {
 	MineBlocksNImmediatelyCancelChan chan bool
 	isMiningImmediately              bool
 	candidateRequestInterval         time.Duration
-	initialBlockFinalWaitDuration    time.Duration
 	maxSubtreeCount                  int
 }
 
@@ -53,12 +52,6 @@ func NewMiner(ctx context.Context, logger ulogger.Logger, blockchainClient block
 	candidateRequestInterval, _ := gocore.Config().GetInt("mine_candidate_request_interval", 10)
 	timeMining := gocore.Config().GetBool("miner_time_mining", false)
 
-	// How long to wait between mining the last few blocks
-	initialBlockFinalWaitDuration, err, _ := gocore.Config().GetDuration("mine_initial_blocks_final_wait", 5*time.Second)
-	if err != nil {
-		return nil, errors.NewServiceError("[NewMiner] failed to get mine_initial_blocks_final_wait", err)
-	}
-
 	maxSubtreeCount, _ := gocore.Config().GetInt("miner_max_subtree_count", 600)
 	maxSubtreeCountVariance, _ := gocore.Config().GetInt("miner_max_subtree_count_variance", 100)
 
@@ -72,13 +65,12 @@ func NewMiner(ctx context.Context, logger ulogger.Logger, blockchainClient block
 	}
 
 	return &Miner{
-		logger:                        logger,
-		blockAssemblyClient:           blockAssemblyClient,
-		candidateRequestInterval:      time.Duration(candidateRequestInterval),
-		timeMining:                    timeMining,
-		initialBlockFinalWaitDuration: initialBlockFinalWaitDuration,
-		maxSubtreeCount:               maxSubtreeCount,
-		blockchainClient:              blockchainClient,
+		logger:                   logger,
+		blockAssemblyClient:      blockAssemblyClient,
+		candidateRequestInterval: time.Duration(candidateRequestInterval),
+		timeMining:               timeMining,
+		maxSubtreeCount:          maxSubtreeCount,
+		blockchainClient:         blockchainClient,
 	}, nil
 }
 
@@ -150,6 +142,7 @@ func (m *Miner) Start(ctx context.Context) error {
 		select {
 		case <-ctx.Done():
 			m.logger.Infof("[Miner] Stopping miner as ctx is done")
+
 			if cancel != nil {
 				cancel()
 			}
@@ -171,6 +164,7 @@ func (m *Miner) Start(ctx context.Context) error {
 			if cancel != nil {
 				cancel()
 			}
+
 			miningCtx, cancel = context.WithCancel(ctx)
 			defer cancel() // Ensure cancel is called at the end of each iteration
 
@@ -185,16 +179,24 @@ func (m *Miner) Start(ctx context.Context) error {
 				continue
 			}
 
+			startHeight, _ := gocore.Config().GetInt("mine_start_height", 0)
+
+			if candidate.Height <= uint32(startHeight) { //nolint:gosec
+				m.logger.Infof("[Miner] Node has not reached start height %d, skipping mining", startHeight)
+				continue
+			}
+
 			if previousCandidate != nil && bytes.Equal(candidate.Id, previousCandidate.Id) {
 				m.logger.Infof("[Miner] Got same candidate as previous, skipping %s", utils.ReverseAndHexEncodeSlice(candidate.Id))
 				m.candidateTimer.Reset(0)
 
 				continue
 			}
-			previousCandidate = candidate
 
+			previousCandidate = candidate
 			waitSeconds := m.waitSeconds
-			if candidate.SubtreeCount > uint32(m.maxSubtreeCount) {
+
+			if candidate.SubtreeCount > uint32(m.maxSubtreeCount) { //nolint:gosec
 				// mine without waiting
 				m.logger.Infof("candidate subtree count (%d) exceeds max subtree count (%d), mining immediately", candidate.SubtreeCount, m.maxSubtreeCount)
 				waitSeconds = 1
@@ -258,8 +260,7 @@ func (m *Miner) mine(ctx context.Context, candidate *model.MiningCandidate, wait
 
 	initialBlockCount, _ := gocore.Config().GetInt("mine_initial_blocks_count", 200)
 
-	if gocore.Config().GetBool("mine_initial_blocks", false) && candidate.Height <= uint32(initialBlockCount) {
-
+	if gocore.Config().GetBool("mine_initial_blocks", false) && candidate.Height <= uint32(initialBlockCount) { //nolint:gosec
 		generateBlocks = true
 	} else {
 		generateBlocks = false
@@ -289,11 +290,6 @@ func (m *Miner) mine(ctx context.Context, candidate *model.MiningCandidate, wait
 		}
 	} else {
 		m.logger.Infof("[Miner] Found block solution %s, submitting", blockHash.String())
-
-		if candidate.Height > uint32(initialBlockCount-5) {
-			m.logger.Infof("[Miner] Waiting %v to allow coinbase splitting to catch up before mining last few initial blocks", m.initialBlockFinalWaitDuration)
-			time.Sleep(m.initialBlockFinalWaitDuration)
-		}
 	}
 
 	err = m.blockAssemblyClient.SubmitMiningSolution(ctx, solution)
@@ -378,8 +374,10 @@ func (m *Miner) mineBlocks(ctx context.Context, blocks int) error {
 }
 
 func (m *Miner) miningCandidate(ctx context.Context, blocks int, previousHash *chainhash.Hash) (*model.MiningCandidate, error) {
-	var candidate *model.MiningCandidate
-	var err error
+	var (
+		candidate *model.MiningCandidate
+		err       error
+	)
 
 	// Initialize backoff parameters
 	minBackoff := 100 * time.Millisecond
@@ -396,6 +394,7 @@ func (m *Miner) miningCandidate(ctx context.Context, blocks int, previousHash *c
 
 		case <-m.MineBlocksNImmediatelyCancelChan:
 			m.logger.Infof("[Miner] Cancelled mining %d blocks immediately", blocks)
+
 			if candidate == nil {
 				return nil, errors.NewServiceError("[Miner] aborting mining on job %s", "unknown")
 			}
