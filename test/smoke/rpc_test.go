ffe858170e9b44e112d1bb8d1303f50ec5834071
rid3thespiral <24756750+rid3thespiral@users.noreply.github.com>
2024-10-21 12:22:51 +0200

    test rpc getinfo

--- test/smoke/rpc_test.go
+++ test/smoke/rpc_test.go
@@ -17,52 +17,6 @@ type RPCTestSuite struct {
 	arrange.TeranodeTestSuite
 }
 
-type BlockchainInfo struct {
-	Result struct {
-		BestBlockHash        string   `json:"bestblockhash"`
-		Blocks               int      `json:"blocks"`
-		Chain                string   `json:"chain"`
-		Chainwork            string   `json:"chainwork"`
-		Difficulty           string   `json:"difficulty"`
-		Headers              int      `json:"headers"`
-		Mediantime           int      `json:"mediantime"`
-		Pruned               bool     `json:"pruned"`
-		Softforks            []string `json:"softforks"`
-		VerificationProgress float64  `json:"verificationprogress"`
-	} `json:"result"`
-	Error interface{} `json:"error"`
-	ID    interface{} `json:"id"`
-}
-
-type P2PNode struct {
-	ID             int    `json:"id"`
-	Addr           string `json:"addr"`
-	Services       string `json:"services"`
-	ServicesStr    string `json:"servicesStr"`
-	RelayTxes      bool   `json:"relaytxes"`
-	LastSend       int    `json:"lastsend"`
-	LastRecv       int    `json:"lastrecv"`
-	BytesSent      int    `json:"bytessent"`
-	BytesRecv      int    `json:"bytesrecv"`
-	ConnTime       int    `json:"conntime"`
-	TimeOffset     int    `json:"timeoffset"`
-	PingTime       int    `json:"pingtime"`
-	Version        int    `json:"version"`
-	SubVer         string `json:"subver"`
-	Inbound        bool   `json:"inbound"`
-	StartingHeight int    `json:"startingheight"`
-	BanScore       int    `json:"banscore"`
-	Whitelisted    bool   `json:"whitelisted"`
-	FeeFilter      int    `json:"feefilter"`
-	SyncNode       bool   `json:"syncnode"`
-}
-
-type P2PRPCResponse struct {
-	Result []P2PNode   `json:"result"`
-	Error  interface{} `json:"error"` // `error` potrebbe essere null o contenere informazioni, quindi usa `interface{}`
-	ID     interface{} `json:"id"`    // `id` potrebbe essere null o un numero, quindi `interface{}`
-}
-
 const (
 	ubsv1RPCEndpoint string = "http://localhost:11292"
 )
@@ -116,6 +70,36 @@ func (suite *RPCTestSuite) TestRPCGetPeerInfo() {
 		t.Errorf("Test failed: peers list is empty")
 	} else {
 		t.Logf("Test succeeded, retrieved P2P peers informations")
+	}
+}
+
+func (suite *RPCTestSuite) TestRPCGetInfo() {
+	t := suite.T()
+
+	var getInfo GetInfo
+
+	resp, err := helper.CallRPC(ubsv1RPCEndpoint, "getinfo", []interface{}{})
+
+	if err != nil {
+		t.Errorf("Error CallRPC: %v", err)
+	}
+
+	errJSON := json.Unmarshal([]byte(resp), &getInfo)
+	if err != nil {
+		t.Errorf("JSON decoding error: %v", errJSON)
+		return
+	}
+
+	t.Logf("%s", resp)
+
+	if getInfo.Error != nil {
+		if strErr, ok := getInfo.Error.(string); ok && strErr == "null" {
+			t.Errorf("Test failed: getinfo RPC call returned error: %v", strErr)
+		} else {
+			t.Errorf("Test failed: getinfo RPC call returned an unexpected error type: %v", getInfo.Error)
+		}
+	} else {
+		t.Logf("Test succeeded, retrieved information from getinfo RPC call")
 	}
 }
 
