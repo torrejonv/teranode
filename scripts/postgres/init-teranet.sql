24e279fc13352178b9d88737cb06d932b33de84a
teracoder <199657068+teracoder@users.noreply.github.com>
2024-11-05 11:12:19 +0000

    [Fix] sql init

---
+++ scripts/postgres/init-teranet.sql
@@ -0,0 +1,61 @@
+
+CREATE ROLE coinbase1 LOGIN
+  PASSWORD 'coinbase1'
+  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+grant coinbase1 to postgresuser;
+CREATE
+DATABASE coinbase1
+  WITH OWNER = coinbase1
+  ENCODING = 'UTF8'
+  CONNECTION LIMIT = -1;
+
+CREATE ROLE coinbase2 LOGIN
+  PASSWORD 'coinbase2'
+  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+grant coinbase2 to postgresuser;
+CREATE
+DATABASE coinbase2
+  WITH OWNER = coinbase2
+  ENCODING = 'UTF8'
+  CONNECTION LIMIT = -1;
+
+CREATE ROLE coinbase3 LOGIN
+  PASSWORD 'coinbase3'
+  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+grant coinbase3 to postgresuser;
+CREATE
+DATABASE coinbase3
+  WITH OWNER = coinbase3
+  ENCODING = 'UTF8'
+  CONNECTION LIMIT = -1;
+
+CREATE ROLE "prod-teranet-1" LOGIN
+  PASSWORD 'prod-teranet-1'
+  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+grant "prod-teranet-1" to postgresuser;
+CREATE
+DATABASE "prod-teranet-1"
+  WITH OWNER = "prod-teranet-1"
+  ENCODING = 'UTF8'
+  CONNECTION LIMIT = -1;
+
+CREATE ROLE "prod-teranet-2" LOGIN
+  PASSWORD 'prod-teranet-2'
+  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+grant "prod-teranet-2" to postgresuser;
+CREATE
+DATABASE "prod-teranet-2"
+  WITH OWNER = "prod-teranet-2"
+  ENCODING = 'UTF8'
+  CONNECTION LIMIT = -1;
+
+CREATE ROLE "prod-teranet-3" LOGIN
+  PASSWORD 'prod-teranet-3'
+  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+grant "prod-teranet-3" to postgresuser;
+CREATE
+DATABASE "prod-teranet-3"
+  WITH OWNER = "prod-teranet-3"
+  ENCODING = 'UTF8'
+  CONNECTION LIMIT = -1;
+
