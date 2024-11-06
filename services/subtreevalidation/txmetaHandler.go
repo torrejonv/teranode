0e8061b3f7e1f12e6fbbcc458216de0d23cb5f5b
Siggi <4411176+icellan@users.noreply.github.com>
2024-11-06 17:07:31 +0100

    Removed message from Kafka txMeta that error out with too big key / value #1549 (#1551)

--- services/subtreevalidation/txmetaHandler.go
+++ services/subtreevalidation/txmetaHandler.go
@@ -20,14 +20,34 @@ func (u *Server) txmetaHandler(msg *kafka.KafkaMessage) error {
 		if bytes.Equal(msg.Value[chainhash.HashSize:], []byte("delete")) {
 			if err := u.DelTxMetaCache(context.Background(), &hash); err != nil {
 				prometheusSubtreeValidationSetTXMetaCacheKafkaErrors.Inc()
-				return errors.NewProcessingError("[txmetaHandler][%s] failed to delete tx meta data", hash, err)
+
+				wrappedErr := errors.NewProcessingError("[txmetaHandler][%s] failed to delete tx meta data", hash, err)
+				if errors.Is(err, errors.ErrProcessing) {
+					// log the wrapped error, instead of throwing an error on Kafka. The message will never be able to be
+					// added to the tx meta cache, so we don't want to keep trying to process it.
+					u.logger.Warnf(wrappedErr.Error())
+
+					return nil
+				}
+
+				return wrappedErr
 			} else {
 				prometheusSubtreeValidationDelTXMetaCacheKafka.Observe(float64(time.Since(startTime).Microseconds()) / 1_000_000)
 			}
 		} else {
 			if err := u.SetTxMetaCacheFromBytes(context.Background(), hash.CloneBytes(), msg.Value[chainhash.HashSize:]); err != nil {
 				prometheusSubtreeValidationSetTXMetaCacheKafkaErrors.Inc()
-				return errors.NewProcessingError("[txmetaHandler][%s] failed to set tx meta data", hash, err)
+
+				wrappedErr := errors.NewProcessingError("[txmetaHandler][%s] failed to set tx meta data", hash, err)
+				if errors.Is(err, errors.ErrProcessing) {
+					// log the wrapped error, instead of throwing an error on Kafka. The message will never be able to be
+					// added to the tx meta cache, so we don't want to keep trying to process it.
+					u.logger.Warnf(wrappedErr.Error())
+
+					return nil
+				}
+
+				return wrappedErr
 			} else {
 				prometheusSubtreeValidationSetTXMetaCacheKafka.Observe(float64(time.Since(startTime).Microseconds()) / 1_000_000)
 			}
