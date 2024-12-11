4d808404449140ece444a1aff9e6711c2e3cb21c
liam <26772+liam@users.noreply.github.com>
2024-12-11 16:23:48 +0000

    Fix lint issue to try a signed commit

--- modules/p2pBootstrap/Server.go
+++ modules/p2pBootstrap/Server.go
@@ -12,6 +12,7 @@ import (
 	"github.com/libp2p/go-libp2p/core/pnet"
 	"github.com/libp2p/go-libp2p/core/protocol"
 	"github.com/ordishs/gocore"
+	"github.com/pkg/errors"
 )
 
 const (
@@ -19,42 +20,46 @@ const (
 )
 
 func main() {
-
 	stats := gocore.Config().Stats()
 	fmt.Printf("STATS\n%s\n", stats)
 
-	dhtProtocolIdStr, ok := gocore.Config().Get("p2p_dht_protocol_id")
+	dhtProtocolIDStr, ok := gocore.Config().Get("p2p_dht_protocol_id")
 	if !ok {
-		panic(fmt.Errorf("error getting p2p_dht_protocol_id"))
+		panic(errors.New("error getting p2p_dht_protocol_id"))
 	}
+
 	sharedKey, ok := gocore.Config().Get("p2p_shared_key")
 	if !ok {
-		panic(fmt.Errorf("error getting p2p_shared_key"))
+		panic(errors.New("error getting p2p_shared_key"))
 	}
+
 	privkeyHex, ok := gocore.Config().Get("p2p_bootstrap_privkey")
 	if !ok {
-		panic(fmt.Errorf("error getting p2p_bootstrap_privkey"))
+		panic(errors.New("error getting p2p_bootstrap_privkey"))
 	}
 
 	listenAddr, ok := gocore.Config().Get("p2p_bootstrap_listenAddress")
 	if !ok {
-		panic(fmt.Errorf("error getting p2p_bootstrap_listenAddress"))
+		panic(errors.New("error getting p2p_bootstrap_listenAddress"))
 	}
 
 	listenPort, ok := gocore.Config().GetInt("p2p_bootstrap_listenPort")
 	if !ok {
-		panic(fmt.Errorf("failed to get p2p_bootstrap_listenPort"))
+		panic(errors.New("failed to get p2p_bootstrap_listenPort"))
 	}
+
 	transportProtocol, ok := gocore.Config().Get("p2p_bootstrap_transportProtocol", "ip4")
 	if !ok {
 		fmt.Printf("failed to get p2p_bootstrap_transportProtocol")
 	}
-	dhtProtocolID := protocol.ID(dhtProtocolIdStr)
+
+	dhtProtocolID := protocol.ID(dhtProtocolIDStr)
 
 	pkBytes, err := hex.DecodeString(privkeyHex)
 	if err != nil {
 		panic(err)
 	}
+
 	pk, err := crypto.UnmarshalPrivateKey(pkBytes)
 	if err != nil {
 		panic(err)
@@ -69,6 +74,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	host, err := libp2p.New(
 		libp2p.ListenAddrStrings(fmt.Sprintf("/%s/%s/tcp/%d", transportProtocol, listenAddr, listenPort)),
 		libp2p.Identity(pk),
@@ -79,6 +85,7 @@ func main() {
 	}
 
 	var options []dht.Option
+
 	mode := dht.ModeServer
 
 	options = append(options, dht.Mode(mode))
@@ -92,6 +99,7 @@ func main() {
 	}
 
 	fmt.Printf("Bootstrap node is running on:")
+
 	for _, addr := range host.Addrs() {
 		fmt.Printf("*  %s/p2p/%s\n", addr, host.ID().String())
 	}
