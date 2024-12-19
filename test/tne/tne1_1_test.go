116ff822af8500efb71cd1b023a78ae8535d3a2c
sugh01 <19183308+sugh01@users.noreply.github.com>
2024-12-19 13:53:35 +0100

    add more steps to tne1 to check for forks with identical TXs (#1845)
    * add more steps to tne1 to check for forks with identical TXs

--- test/tne/tne1_1_test.go
+++ test/tne/tne1_1_test.go
@@ -1,15 +1,15 @@
 //go:build test_all || test_tne || test_functional
 
+package tne
+
 // How to run this test:
 // $ cd test/tne/
 // $ go test -v -run "^TestTNE1_1TestSuite$/TestNode_DoNotVerifyTransactionsIfAlreadyVerified$" -tags test_functional
-
-package tne
-
 import (
 	"testing"
 	"time"
 
+	"github.com/bitcoin-sv/ubsv/services/blockchain/blockchain_api"
 	helper "github.com/bitcoin-sv/ubsv/test/utils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -45,7 +45,10 @@ func (suite *TNE1_1TestSuite) TestNode_DoNotVerifyTransactionsIfAlreadyVerified(
 	blockchainNode1 := framework.Nodes[1].BlockchainClient
 	ctx := framework.Context
 
+	settingsMap["SETTINGS_CONTEXT_1"] = "docker.ubsv1.test.stopP2P"
 	settingsMap["SETTINGS_CONTEXT_2"] = "docker.ubsv2.test.stopP2P"
+	settingsMap["SETTINGS_CONTEXT_3"] = "docker.ubsv3.test.stopP2P"
+
 	if err := framework.RestartDockerNodes(settingsMap); err != nil {
 		t.Errorf("Failed to restart nodes: %v", err)
 	}
@@ -74,31 +77,76 @@ func (suite *TNE1_1TestSuite) TestNode_DoNotVerifyTransactionsIfAlreadyVerified(
 	suite.T().Log("All nodes ready")
 
 	for i := 0; i < 5; i++ {
-		hashes, err := helper.CreateAndSendTxs(ctx, framework.Nodes[0], 1)
-		if err != nil {
-			t.Errorf("Failed to create and send raw txs: %v", err)
-		}
+		for node := 0; node < 3; node++ {
+			hashes, err := helper.CreateAndSendTxs(ctx, framework.Nodes[node], 1)
+			if err != nil {
+				t.Errorf("Failed to create and send raw txs: %v", err)
+			}
 
-		logger.Infof("Hashes: %v", hashes)
+			logger.Infof("Hashes: %v", hashes)
 
-		_, err = helper.MineBlockWithRPC(ctx, framework.Nodes[0], logger)
-		require.NoError(t, err)
+			_, err = helper.MineBlockWithRPC(ctx, framework.Nodes[node], logger)
+			require.NoError(t, err)
 
-		_, err = helper.MineBlockWithRPC(ctx, framework.Nodes[0], logger)
-		require.NoError(t, err)
-
-		if err != nil {
-			t.Errorf("Failed to mine block: %v", err)
+			if err != nil {
+				t.Errorf("Failed to mine block: %v", err)
+			}
 		}
 	}
 
+	settingsMap["SETTINGS_CONTEXT_1"] = "docker.ubsv1.test"
 	settingsMap["SETTINGS_CONTEXT_2"] = "docker.ubsv2.test"
+	settingsMap["SETTINGS_CONTEXT_3"] = "docker.ubsv3.test"
+
 	if err := framework.RestartDockerNodes(settingsMap); err != nil {
 		t.Errorf("Failed to restart nodes: %v", err)
 	}
 
+	// Wait for nodes to be healthy
+	for index, port := range ports {
+		suite.T().Logf("Waiting for node %d to be ready", index)
+
+		err = helper.WaitForHealthLiveness(port, 30*time.Second)
+		if err != nil {
+			suite.T().Fatal(err)
+		}
+	}
+
+	// Wait for all nodes to reach RUNNING state
+	timeout := time.After(30 * time.Second)
+	ticker := time.NewTicker(500 * time.Millisecond)
+
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-timeout:
+			t.Fatal("Timeout waiting for nodes to reach RUNNING state")
+		case <-ticker.C:
+			allRunning := true
+
+			for i, node := range framework.Nodes {
+				state := node.BlockchainClient.GetFSMCurrentStateForE2ETestMode()
+				logger.Infof("Node %d state: %s", i, state)
+
+				if state != blockchain_api.FSMStateType_RUNNING {
+					allRunning = false
+					break
+				}
+			}
+
+			if allRunning {
+				logger.Infof("All nodes are in RUNNING state")
+				goto checkHeaders
+			}
+		}
+	}
+
+checkHeaders:
 	headerNode1, _, _ := blockchainNode1.GetBestBlockHeader(ctx)
+
 	headerNode0, _, _ := blockchainNode0.GetBestBlockHeader(ctx)
+
 	assert.Equal(t, headerNode0.Hash(), headerNode1.Hash(), "Best block headers are not equal")
 }
 
