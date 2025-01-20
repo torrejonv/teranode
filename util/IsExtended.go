c65ea71d4c7375c2ae8cb5459c20cc42f193c13e
oskarszoon <1449115+oskarszoon@users.noreply.github.com>
2025-01-20 17:29:10 +0100

    Remove restrictive check on number of previous tx scripts (#1978)
    Remove restrictive check on number of previous tx scripts, causes issues with IBD

--- util/IsExtended.go
+++ util/IsExtended.go
@@ -12,7 +12,10 @@ func IsExtended(tx *bt.Tx, blockHeight uint32) bool {
 	}
 
 	for _, input := range tx.Inputs {
-		if input.PreviousTxScript == nil || len(*input.PreviousTxScript) == 0 {
+		// on both testnet and mainnet there are outputs with empty scripts
+		// that get spent later, e.g. `8d1c472b6584844c204cb546a3ad6bfd924b4816bc886d6693b5a93888fc019e`
+		//  || len(*input.PreviousTxScript) == 0
+		if input.PreviousTxScript == nil {
 			return false
 		}
 	}
