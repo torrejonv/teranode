fe266a7627a0ed3c9479eb0c1d050048b5a88163
teracoder <199657068+teracoder@users.noreply.github.com>
2025-03-12 13:13:13 +0000

    [Remove] settings migrated to argo (#2317)

---
+++ scripts/postgres/init-stage-eks-1-teranet.sql
@@ -0,0 +1,61 @@
+
+CREATE ROLE "coinbase-stage-eks-1-teranet-1" LOGIN
+  PASSWORD 'coinbase-stage-eks-1-teranet-1'
+  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+grant "coinbase-stage-eks-1-teranet-1" to postgresuser;
+CREATE
+DATABASE "coinbase-stage-eks-1-teranet-1"
+  WITH OWNER = "coinbase-stage-eks-1-teranet-1"
+  ENCODING = 'UTF8'
+  CONNECTION LIMIT = -1;
+
+CREATE ROLE "coinbase-stage-eks-1-teranet-2" LOGIN
+  PASSWORD 'coinbase-stage-eks-1-teranet-2'
+  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+grant "coinbase-stage-eks-1-teranet-2" to postgresuser;
+CREATE
+DATABASE "coinbase-stage-eks-1-teranet-2"
+  WITH OWNER = "coinbase-stage-eks-1-teranet-2"
+  ENCODING = 'UTF8'
+  CONNECTION LIMIT = -1;
+
+CREATE ROLE "coinbase-stage-eks-1-teranet-3" LOGIN
+  PASSWORD 'coinbase-stage-eks-1-teranet-3'
+  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+grant "coinbase-stage-eks-1-teranet-3" to postgresuser;
+CREATE
+DATABASE "coinbase-stage-eks-1-teranet-3"
+  WITH OWNER = "coinbase-stage-eks-1-teranet-3"
+  ENCODING = 'UTF8'
+  CONNECTION LIMIT = -1;
+
+CREATE ROLE "stage-eks-1-teranet-1" LOGIN
+  PASSWORD 'stage-eks-1-teranet-1'
+  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+grant "stage-eks-1-teranet-1" to postgresuser;
+CREATE
+DATABASE "stage-eks-1-teranet-1"
+  WITH OWNER = "stage-eks-1-teranet-1"
+  ENCODING = 'UTF8'
+  CONNECTION LIMIT = -1;
+
+CREATE ROLE "stage-eks-1-teranet-2" LOGIN
+  PASSWORD 'stage-eks-1-teranet-2'
+  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+grant "stage-eks-1-teranet-2" to postgresuser;
+CREATE
+DATABASE "stage-eks-1-teranet-2"
+  WITH OWNER = "stage-eks-1-teranet-2"
+  ENCODING = 'UTF8'
+  CONNECTION LIMIT = -1;
+
+CREATE ROLE "stage-eks-1-teranet-3" LOGIN
+  PASSWORD 'stage-eks-1-teranet-3'
+  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+grant "stage-eks-1-teranet-3" to postgresuser;
+CREATE
+DATABASE "stage-eks-1-teranet-3"
+  WITH OWNER = "stage-eks-1-teranet-3"
+  ENCODING = 'UTF8'
+  CONNECTION LIMIT = -1;
+
