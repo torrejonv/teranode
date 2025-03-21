1b104660a5b8253e95c15c6259e41bbc7cdc8c39
rid3thespiral <24756750+rid3thespiral@users.noreply.github.com>
2025-03-21 13:20:57 +0100

    [TEST] Cleanup sync-test and improved TNC2.1 (#2378)

--- test/tnc/tnc2_1_test.go
+++ test/tnc/tnc2_1_test.go
@@ -25,7 +25,6 @@ package tnc
 import (
 	"sync"
 	"testing"
-	"time"
 
 	helper "github.com/bitcoin-sv/teranode/test/utils"
 	"github.com/bitcoin-sv/teranode/test/utils/tconfig"
@@ -109,13 +108,13 @@ func (suite *TNC2_1TestSuite) TestConcurrentCandidateIdentifiers() {
 	candidateIds := make(chan []byte, numRequests)
 
 	var wg sync.WaitGroup
+	wg.Add(1)
 
-	// Request mining candidates concurrently
 	for i := 0; i < numRequests; i++ {
 		wg.Add(1)
-
 		go func() {
 			defer wg.Done()
+			wg.Wait() // Aspetta il segnale per partire
 
 			mc, err := helper.GetMiningCandidate(ctx, node.BlockassemblyClient, logger)
 			require.NoError(t, err, "Failed to get mining candidate in concurrent request")
@@ -125,8 +124,8 @@ func (suite *TNC2_1TestSuite) TestConcurrentCandidateIdentifiers() {
 		}()
 	}
 
-	// Small delay to ensure we're not getting the same candidate
-	time.Sleep(100 * time.Millisecond)
+	// Rilascia tutte le goroutine contemporaneamente
+	wg.Done()
 
 	wg.Wait()
 	close(candidateIds)
